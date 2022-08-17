94d777e5650c8699791489f6b40155a0b0daef22
konstantin <kvengerov@gmail.com>
2022-08-17 16:15:58 +0300

    feat: work on api, get permissions, roles

--- src/app/core/interfaces/index.ts
+++ src/app/core/interfaces/index.ts
@@ -1,5 +1,5 @@
 export { MenuInterface, UserMenuInterface } from './menu.interface';
-export { Breadcrumb } from './breadcrumb';
+export { Breadcrumb } from './breadcrumb.interface';
 export * from './session.interface';
 export * from './country-code.interface';
 export * from './role.interface';
