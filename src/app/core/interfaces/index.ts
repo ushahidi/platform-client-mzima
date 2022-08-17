43e3f584284b48a9c5f1c656f94eb5f33bc6ec26
konstantin <kvengerov@gmail.com>
2022-08-17 16:05:59 +0300

    feat: work on api, get permissions, roles

--- src/app/core/interfaces/index.ts
+++ src/app/core/interfaces/index.ts
@@ -3,4 +3,3 @@ export * from './breadcrumb';
 export * from './country-code.interface';
 export * from './role.interface';
 export * from './permission.interface';
-export * from './api-result.interface';
