84b6f2bd85d5dfc89ac696ef7042caf019dd1e44
Ifycode <marychord@gmail.com>
2022-10-27 01:45:04 +0100

    Display metadata info though post metadata component

--- src/app/post/post-metadata/post-metadata.component.ts
+++ src/app/post/post-metadata/post-metadata.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { PostPropertiesInterface, PostResult } from '@models';
 
 @Component({
   selector: 'app-post-metadata',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-metadata.component.scss'],
 })
 export class PostMetadataComponent implements OnInit {
+  @Input() post: PostResult | PostPropertiesInterface;
+
   // eslint-disable-next-line @typescript-eslint/no-useless-constructor, @typescript-eslint/no-empty-function
   constructor() {}
 
