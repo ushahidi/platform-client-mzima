37a5b4c2614f81dac7960b9843286e69e47f9129
Obiagba Mary Ifeoma <marychord@gmail.com>
2022-10-28 08:14:06 +0100

    Fix: All post cards are anonymous when not loggedIn (#52)

--- src/app/post/post-metadata/post-metadata.component.ts
+++ src/app/post/post-metadata/post-metadata.component.ts
@@ -8,11 +8,28 @@ import { PostPropertiesInterface, PostResult } from '@models';
 })
 export class PostMetadataComponent implements OnInit {
   @Input() post: PostResult | PostPropertiesInterface;
+  author: string;
 
   // eslint-disable-next-line @typescript-eslint/no-useless-constructor, @typescript-eslint/no-empty-function
   constructor() {}
 
   ngOnInit(): void {
     console.log('Post metadata component!');
+  }
+
+  showAuthor() {
+    const authorNameOrContact =
+      this.post.user?.realname || this.post.contact?.contact || this.post.author_realname;
+
+    if (authorNameOrContact === undefined) {
+      return false;
+    }
+
+    if (authorNameOrContact !== undefined) {
+      this.author = authorNameOrContact ? authorNameOrContact : 'Anonymous';
+      return true;
+    }
+
+    return;
   }
 }
