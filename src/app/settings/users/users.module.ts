68f48428a396d31893b37ec544815dad3b1a773e
konstantin <kvengerov@gmail.com>
2022-11-04 11:58:06 +0200

    added update & delete user profile

--- src/app/settings/users/users.module.ts
+++ src/app/settings/users/users.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MaterialModule, SharedModule } from '../../shared';
+import { MaterialModule, SharedModule } from '@shared';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
