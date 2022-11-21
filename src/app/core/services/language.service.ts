7068efaf4a7c0fc31615a7a921d9c30dab4960d0
konstantin <kvengerov@gmail.com>
2022-11-21 13:05:00 +0200

    fix translate service

--- src/app/core/services/language.service.ts
+++ src/app/core/services/language.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { CONST } from '@constants';
+import { TranslateService } from '@ngx-translate/core';
+import { BehaviorSubject } from 'rxjs';
 import LangJSON from '../../../assets/locales/languages.json';
 import { LanguageInterface } from '@models';
 
@@ -6,6 +9,26 @@ import { LanguageInterface } from '@models';
   providedIn: 'root',
 })
 export class LanguageService {
+  private languages = new BehaviorSubject<LanguageInterface[]>(this.getLanguages());
+  public languages$ = this.languages.asObservable();
+
+  private languageKey = `${CONST.LOCAL_STORAGE_PREFIX}language`;
+
+  private initialLanguage = localStorage.getItem(this.languageKey);
+  private selectedLanguage = new BehaviorSubject<any>('');
+  public selectedLanguage$ = this.selectedLanguage.asObservable();
+
+  constructor(private translate: TranslateService) {
+    if (this.initialLanguage === 'null' || this.initialLanguage === null) {
+      this.initialLanguage = 'en';
+      this.selectedLanguage.next('en');
+      localStorage.setItem(this.languageKey, this.initialLanguage);
+      this.setLanguage(this.initialLanguage);
+    } else {
+      this.setLanguage(this.initialLanguage);
+    }
+  }
+
   getLanguages(): LanguageInterface[] {
     if (LangJSON.languages && LangJSON.languages.length > 0) {
       return LangJSON.languages;
@@ -20,5 +43,17 @@ export class LanguageService {
         },
       ];
     }
+  }
+
+  setLanguage(lang: string) {
+    this.translate.setDefaultLang(lang);
+    this.translate.use(lang);
+    this.selectedLanguage.next(lang);
+  }
+
+  changeLanguage(value: string) {
+    this.translate.use(value);
+    localStorage.setItem(this.languageKey, value);
+    this.selectedLanguage.next(value);
   }
 }
