ae73ca3c16539f7ee1024460c422460db3b48b03
konstantin <kvengerov@gmail.com>
2022-11-23 11:13:58 +0200

    fix create post functionality

--- src/app/post/create/create.component.ts
+++ src/app/post/create/create.component.ts
@@ -1,11 +1,15 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { ActivatedRoute, Router } from '@angular/router';
+import { GeoJsonFilter, PostResult } from '@models';
 import {
   ConfirmModalService,
   EventBusService,
   EventType,
+  PostsService,
   PostsV5Service,
   SurveysService,
 } from '@services';
@@ -25,23 +29,30 @@ dayjs.extend(timezone);
 })
 export class CreateComponent implements OnInit {
   public data: any;
-  public fields: any[] = [];
   public form: FormGroup;
   public description: string;
   public title: string;
   private formId?: number;
-  public tasksFormArray = new FormArray([]);
   public tasks: any[];
   public activeLanguage: string;
-  public initialFormData: any;
+  private initialFormData: any;
+  private relationConfigForm: any;
+  private relationConfigKey: string;
+  private isSearching = false;
+  public relatedPosts: PostResult[];
+  public relationSearch: string;
+  public selectedRelatedPost: any;
+  private completeStages: number[] = [];
+  private fieldsFormArray = ['tags'];
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public type: number,
     private matDialogRef: MatDialogRef<CreateComponent>,
     private route: ActivatedRoute,
     private surveysService: SurveysService,
-    // private formBuilder: FormBuilder,
+    private formBuilder: FormBuilder,
     private postsV5Service: PostsV5Service,
+    private postsService: PostsService,
     private router: Router,
     private translate: TranslateService,
     private confirmModalService: ConfirmModalService,
@@ -51,11 +62,6 @@ export class CreateComponent implements OnInit {
   ngOnInit(): void {
     this.formId = this.type;
     this.loadData(this.formId);
-    // this.route.paramMap.subscribe((params) => {
-    //   this.formId = Number(params.get('id'));
-    //   this.loadData(this.formId);
-    // });
-
     this.translate.onLangChange.subscribe((newLang) => {
       this.activeLanguage = newLang.lang;
     });
@@ -68,9 +74,7 @@ export class CreateComponent implements OnInit {
         this.data = data;
         this.tasks = data.result.tasks;
 
-        // const arr: [] = [];
         let fields: any = {};
-
         for (const task of this.tasks) {
           task.fields
             .sort((a: any, b: any) => a.priority - b.priority)
@@ -80,6 +84,10 @@ export class CreateComponent implements OnInit {
               }
               if (field.type === 'description') {
                 this.description = field.default;
+              }
+              if (field.type === 'relation') {
+                this.relationConfigForm = field.config.input.form;
+                this.relationConfigKey = field.key;
               }
 
               if (field.key) {
@@ -93,134 +101,136 @@ export class CreateComponent implements OnInit {
                     ? 0
                     : '');
                 field.value = value;
-                // this.addTaskFormGroup(
-                //   field.key,
-                //   value,
-                //   field.required ? Validators.required : null,
-                // );
-                fields[field.key] = new FormControl(
-                  value,
-                  field.required ? Validators.required : null,
-                );
+                fields[field.key] = this.fieldsFormArray.includes(field.type)
+                  ? this.addFormArray(value, field)
+                  : this.addFormControl(value, field);
               }
             });
         }
         this.form = new FormGroup(fields);
-        console.log(this.form);
-        console.log(this.form.value);
         this.initialFormData = this.form.value;
       },
     });
+  }
+
+  private addFormArray(value: string, field: any) {
+    return this.formBuilder.array(
+      [] || [new FormControl(value)],
+      field.required ? Validators.required : null,
+    );
+  }
+
+  private addFormControl(value: string, field: any) {
+    return new FormControl(value, field.required ? Validators.required : null);
   }
 
   public getOptionsByParentId(field: any, parent_id: number): any[] {
     return field.options.filter((option: any) => option.parent_id === parent_id);
   }
 
-  public async submitPost(): Promise<void> {
-    console.log(this.form.value);
-    // if (this.form.disabled) return;
-    //
-    // this.form.disable();
-    //
-    // const fields = this.fields.map((field) => {
-    //   const value: any = {
-    //     value: this.form.value[field.key],
-    //   };
-    //
-    //   switch (field.input) {
-    //     case 'date':
-    //     case 'datetime':
-    //       value.value_meta = {
-    //         from_tz: dayjs.tz.guess(),
-    //       };
-    //       break;
-    //     case 'location':
-    //       value.value = {
-    //         lat: this.form.value[field.key].lat,
-    //         lon: this.form.value[field.key].lng,
-    //       };
-    //       break;
-    //     case 'tags':
-    //     case 'checkbox':
-    //       value.value = this.form.value[field.key] || [];
-    //       break;
-    //   }
-    //
-    //   return {
-    //     ...field,
-    //     value,
-    //   };
-    // });
-    //
-    // const postData = {
-    //   allowed_privileges: [
-    //     'read',
-    //     'create',
-    //     'update',
-    //     'delete',
-    //     'search',
-    //     'change_status',
-    //     'read_full',
-    //   ],
-    //   base_language: 'en',
-    //   completed_stages: [],
-    //   content: this.description,
-    //   description: '',
-    //   enabled_languages: {},
-    //   form_id: this.formId,
-    //   locale: 'en_US',
-    //   post_content: [
-    //     {
-    //       fields,
-    //       description: null,
-    //       form_id: this.formId,
-    //       id: 2,
-    //       label: 'Structure',
-    //       priority: 0,
-    //       required: false,
-    //       show_when_published: true,
-    //       task_is_internal_only: false,
-    //       translations: [],
-    //       type: 'post',
-    //     },
-    //   ],
-    //   post_date: new Date().toISOString(),
-    //   published_to: [],
-    //   title: this.title,
-    //   type: 'report',
-    // };
-    // this.postsV5Service.post(postData).subscribe({
-    //   error: () => {
-    //     this.form.enable();
-    //   },
-    //   complete: async () => {
-    //     this.matDialogRef.close();
-    //     this.form.enable();
-    //     await this.confirmModalService.open({
-    //       title: this.translate.instant('notify.confirm_modal.add_post_success.success'),
-    //       description: `<p>${this.translate.instant(
-    //         'notify.confirm_modal.add_post_success.success_description',
-    //       )}</p>`,
-    //       buttonSuccess: this.translate.instant(
-    //         'notify.confirm_modal.add_post_success.success_button',
-    //       ),
-    //     });
-    //   },
-    // });
+  preparationData(): any {
+    for (const task of this.tasks) {
+      task.fields = task.fields.map(
+        (field: { key: string | number; input: string; type: string }) => {
+          let value: any = {
+            value: this.form.value[field.key],
+          };
+
+          if (field.type === 'title') this.title = this.form.value[field.key];
+          if (field.type === 'description') this.description = this.form.value[field.key];
+
+          switch (field.input) {
+            case 'date':
+            case 'datetime':
+              value = this.form.value[field.key]
+                ? {
+                    value: dayjs(this.form.value[field.key]).format('YYYY-MM-DD'),
+                    value_meta: {
+                      from_tz: dayjs.tz.guess(),
+                    },
+                  }
+                : { value: null };
+              break;
+            case 'location':
+              value.value = {
+                lat: this.form.value[field.key].lat,
+                lon: this.form.value[field.key].lng,
+              };
+              break;
+            case 'tags':
+            case 'checkbox':
+              value.value = this.form.value[field.key] || [];
+              break;
+            case 'relation':
+            case 'video':
+              value = this.form.value[field.key]
+                ? {
+                    value: this.form.value[field.key],
+                  }
+                : {};
+              break;
+            case 'upload':
+              value.value = this.form.value[field.key] || null;
+              break;
+          }
+
+          return {
+            ...field,
+            value,
+          };
+        },
+      );
+    }
   }
 
-  public onCheckChange(event: any, field: string) {
-    const formArray: FormArray = this.form.get(field) as FormArray;
+  public async submitPost(): Promise<void> {
+    if (this.form.disabled) return;
+    this.form.disable();
 
-    if (event.checked) {
-      formArray.push(new FormControl(event.source.value));
-    } else {
-      const index = formArray.controls.findIndex((ctrl: any) => ctrl.value === event.source.value);
-      if (index > -1) {
-        formArray.removeAt(index);
-      }
-    }
+    this.preparationData();
+
+    const postData = {
+      allowed_privileges: [
+        'read',
+        'create',
+        'update',
+        'delete',
+        'search',
+        'change_status',
+        'read_full',
+      ],
+      base_language: 'en',
+      completed_stages: this.completeStages,
+      content: this.description,
+      description: '',
+      enabled_languages: {},
+      form_id: this.formId,
+      locale: 'en_US',
+      post_content: this.tasks,
+      post_date: new Date().toISOString(),
+      published_to: [],
+      title: this.title,
+      type: 'report',
+    };
+
+    this.postsV5Service.post(postData).subscribe({
+      error: () => {
+        this.form.enable();
+      },
+      complete: async () => {
+        this.matDialogRef.close();
+        this.form.enable();
+        await this.confirmModalService.open({
+          title: this.translate.instant('notify.confirm_modal.add_post_success.success'),
+          description: `<p>${this.translate.instant(
+            'notify.confirm_modal.add_post_success.success_description',
+          )}</p>`,
+          buttonSuccess: this.translate.instant(
+            'notify.confirm_modal.add_post_success.success_button',
+          ),
+        });
+      },
+    });
   }
 
   public async previousPage() {
@@ -243,27 +253,78 @@ export class CreateComponent implements OnInit {
     });
   }
 
-  toggleAllSelection(fieldKey: string) {
-    console.log(this.form.controls[fieldKey].value.includes('all'));
-    if (this.form.controls[fieldKey].value.includes('all')) {
-      for (const task of this.tasks) {
-        task.fields.map((field: any) => {
-          if (field.key === fieldKey) {
-            const ids = field.options.map((el: any) => el.id);
-            this.form.controls[fieldKey].setValue(['all', ...ids]);
-          }
+  public toggleAllSelection(event: MatCheckboxChange, fields: any, fieldKey: string) {
+    fields.map((field: any) => {
+      if (field.key === fieldKey) {
+        field.options.map((el: any) => {
+          this.onCheckChange(event, field.key, el.id);
         });
       }
+    });
+  }
+
+  public onCheckChange(event: any, fieldKey: string, id: number) {
+    const formArray: FormArray = this.form.get(fieldKey) as FormArray;
+    if (event.checked) {
+      const index = formArray.controls.findIndex((ctrl: any) => ctrl.value === id);
+      if (index === -1) formArray.push(new FormControl(id));
     } else {
-      this.form.controls[fieldKey].setValue([]);
+      const index = formArray.controls.findIndex((ctrl: any) => ctrl.value === id);
+      if (index > -1) {
+        formArray.removeAt(index);
+      }
     }
   }
 
-  trackById(item: any): number {
-    return item.id;
+  updateAllComplete(fieldKey: string, id: number) {
+    return this.form.get(fieldKey)?.value.includes(id);
   }
 
-  relationSearch() {
-    console.log('relationSearch');
+  public relationSearchPosts() {
+    const params: GeoJsonFilter = {
+      order: 'desc',
+      'form[]': this.relationConfigForm,
+      orderby: 'post_date',
+      q: this.relationSearch,
+      'status[]': [],
+    };
+    this.isSearching = true;
+    this.postsService.getPosts('', params).subscribe({
+      next: (data) => {
+        console.log(this.relatedPosts);
+        this.relatedPosts = data.results;
+        this.isSearching = false;
+      },
+      error: () => (this.isSearching = false),
+    });
+  }
+
+  public taskComplete({ id }: any, event: MatSlideToggleChange) {
+    if (event.checked) {
+      this.completeStages.push(id);
+    } else {
+      const index = this.completeStages.indexOf(id);
+      if (index >= 0) {
+        this.completeStages.splice(index, 1);
+      }
+    }
+  }
+
+  public chooseRelatedPost(event: MatCheckboxChange, { key }: any, { id, title }: any) {
+    if (!event.checked) return;
+    this.form.patchValue({ [key]: id });
+    this.selectedRelatedPost = { id, title };
+    this.relatedPosts = [];
+  }
+
+  public deleteRelatedPost({ key }: any, { id }: any) {
+    if (this.form.controls[key].value === id) {
+      this.form.patchValue({ [key]: '' });
+    }
+    this.selectedRelatedPost = null;
+  }
+
+  public trackById(item: any): number {
+    return item.id;
   }
 }
