54d3d6a2d0c4c1d856fe9022920e984f76fddf3e
Konstantin Vengerov <kvengerov@gmail.com>
2023-01-20 09:19:31 +0200

    [MZI-20] Users search/filters for list of users (#97)
    * [MZI-20] Pagination, sorting table, lazy loading data table
    
    * [MZI-20] Pagination, sorting table, lazy loading data table

--- src/app/settings/users/users.module.ts
+++ src/app/settings/users/users.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatSortModule } from '@angular/material/sort';
 import { SharedModule } from '@shared';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { SliderModule } from 'primeng/slider';
+import { TableModule } from 'primeng/table';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
@@ -9,6 +18,20 @@ import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   declarations: [UsersComponent, UserItemComponent],
-  imports: [CommonModule, UsersRoutingModule, FormsModule, SharedModule],
+  imports: [
+    CommonModule,
+    UsersRoutingModule,
+    FormsModule,
+    SharedModule,
+    MatSortModule,
+    NgxPaginationModule,
+    ProgressBarModule,
+    TableModule,
+    InputTextModule,
+    MultiSelectModule,
+    DropdownModule,
+    SliderModule,
+    ButtonModule,
+  ],
 })
 export class UsersModule {}
