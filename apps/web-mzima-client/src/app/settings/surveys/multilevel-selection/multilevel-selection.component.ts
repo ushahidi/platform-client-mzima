30b0da139aff233cb3177c2e169510d664014be5
Dmitry Fast <df@proverstka.com>
2023-03-17 15:02:20 +0200

    ush-240 After selecting parent checkbox, children checkboxes are not being automatically selected for 'Categories' field

--- apps/web-mzima-client/src/app/settings/surveys/multilevel-selection/multilevel-selection.component.ts
+++ apps/web-mzima-client/src/app/settings/surveys/multilevel-selection/multilevel-selection.component.ts
@@ -103,14 +103,12 @@ export class MultilevelSelectionComponent implements ControlValueAccessor, OnCha
     }
   }
 
-  public descendantsAllSelected(option: CategoryFlatNode): boolean {
+  public descendantsHasSelected(option: CategoryFlatNode): boolean {
     const descendants = this.treeControl.getDescendants(option);
-    const descAllSelected =
+    const descHasSelected =
       descendants.length > 0 &&
-      descendants.every((child) => {
-        return this.checklistSelection.isSelected(child);
-      });
-    return descAllSelected;
+      !!descendants.find((child) => this.checklistSelection.isSelected(child));
+    return descHasSelected;
   }
 
   public categorySelectionToggle(node: CategoryFlatNode): void {
@@ -163,18 +161,15 @@ export class MultilevelSelectionComponent implements ControlValueAccessor, OnCha
   private getLevel = (option: CategoryFlatNode) => option.level;
 
   private checkRootNodeSelection(option: CategoryFlatNode): void {
-    const nodeSelected = this.checklistSelection.isSelected(option);
     const descendants = this.treeControl.getDescendants(option);
-    const descAllSelected =
+
+    const descHasSelected =
       descendants.length > 0 &&
-      descendants.every((child) => {
-        return this.checklistSelection.isSelected(child);
-      });
-    if (nodeSelected && !descAllSelected) {
-      this.checklistSelection.deselect(option);
-    } else if (!nodeSelected && descAllSelected) {
-      this.checklistSelection.select(option);
-    }
+      !!descendants.find((child) => this.checklistSelection.isSelected(child));
+
+    descHasSelected
+      ? this.checklistSelection.select(option)
+      : this.checklistSelection.deselect(option);
   }
 
   public toggleSelectAll(): void {
