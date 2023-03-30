0c38b7a0e4a44fecb4f9b47835736c447e3f4a36
konstantin <kvengerov@gmail.com>
2023-03-30 13:48:20 +0300

    [USH-295] Surveys: Responses to Survey fields not visible.

--- apps/web-mzima-client/src/app/post/post-edit/post-edit.component.ts
+++ apps/web-mzima-client/src/app/post/post-edit/post-edit.component.ts
@@ -177,6 +177,30 @@ export class PostEditComponent implements OnInit, OnChanges {
     value.forEach((val: { id: any }) => formArray.push(new FormControl(val?.id)));
   }
 
+  private handleText(key: string, value: any) {
+    this.form.patchValue({ [key]: value?.value });
+  }
+
+  private handleUpload(key: string, value: any) {
+    this.form.patchValue({ [key]: value?.value });
+  }
+
+  private handleVideo(key: string, value: any) {
+    this.form.patchValue({ [key]: value?.value });
+  }
+
+  private handleTextarea(key: string, value: any) {
+    this.form.patchValue({ [key]: value?.value });
+  }
+
+  private handleRelation(key: string, value: any) {
+    this.form.patchValue({ [key]: value?.value });
+  }
+
+  private handleNumber(key: string, value: any) {
+    this.form.patchValue({ [key]: value?.value });
+  }
+
   private handleCheckbox(key: string, value: any) {
     const data = value.map((val: { id: any }) => val?.id);
     this.form.patchValue({ [key]: data });
@@ -203,7 +227,19 @@ export class PostEditComponent implements OnInit, OnChanges {
   }
 
   private updateForm(updateValues: any[]) {
-    type InputHandlerType = 'tags' | 'checkbox' | 'location' | 'date' | 'datetime' | 'radio';
+    type InputHandlerType =
+      | 'tags'
+      | 'checkbox'
+      | 'location'
+      | 'date'
+      | 'datetime'
+      | 'radio'
+      | 'text'
+      | 'upload'
+      | 'video'
+      | 'textarea'
+      | 'relation'
+      | 'number';
     type TypeHandlerType = 'title' | 'description';
 
     const inputHandlers: { [key in InputHandlerType]: (key: string, value: any) => void } = {
@@ -213,6 +249,12 @@ export class PostEditComponent implements OnInit, OnChanges {
       date: this.handleDate.bind(this),
       datetime: this.handleDate.bind(this),
       radio: this.handleRadio.bind(this),
+      text: this.handleText.bind(this),
+      upload: this.handleUpload.bind(this),
+      video: this.handleVideo.bind(this),
+      textarea: this.handleTextarea.bind(this),
+      relation: this.handleRelation.bind(this),
+      number: this.handleNumber.bind(this),
     };
 
     const typeHandlers: { [key in TypeHandlerType]: (key: string) => void } = {
@@ -242,13 +284,14 @@ export class PostEditComponent implements OnInit, OnChanges {
   }
 
   private addFormControl(value: string, field: any): FormControl {
-    // console.log({ field, value });
     if (field.type === 'title') {
       return new FormControl(value, [Validators.required, Validators.minLength(2)]);
     } else if (field.input === 'video') {
       const validators = [];
-      if (field.required) validators.push(Validators.required);
-      validators.push(this.formValidator.videoValidator);
+      if (field.required) {
+        validators.push(Validators.required);
+        validators.push(this.formValidator.videoValidator);
+      }
       return new FormControl(value, validators);
     } else {
       return new FormControl(value, field.required ? Validators.required : null);
@@ -338,25 +381,27 @@ export class PostEditComponent implements OnInit, OnChanges {
     if (!this.form.valid) this.form.markAllAsTouched();
     this.preventSubmitIncaseTheresNoBackendValidation();
 
-    if (this.postId) {
-      postData.post_date = this.post.post_date || new Date().toISOString();
-      this.postsV5Service.update(this.postId, postData).subscribe({
-        error: () => this.form.enable(),
-        complete: async () => {
-          await this.postComplete();
-        },
-      });
-    } else {
-      if (!this.atLeastOneFieldHasValidationError) {
-        this.postsV5Service.post(postData).subscribe({
-          error: () => this.form.enable(),
-          complete: async () => {
-            // console.log('Submit possible!');
-            await this.postComplete();
-          },
-        });
-      }
-    }
+    console.log('postData', postData);
+
+    // if (this.postId) {
+    //   postData.post_date = this.post.post_date || new Date().toISOString();
+    //   this.postsV5Service.update(this.postId, postData).subscribe({
+    //     error: () => this.form.enable(),
+    //     complete: async () => {
+    //       await this.postComplete();
+    //     },
+    //   });
+    // } else {
+    //   if (!this.atLeastOneFieldHasValidationError) {
+    //     this.postsV5Service.post(postData).subscribe({
+    //       error: () => this.form.enable(),
+    //       complete: async () => {
+    //         // console.log('Submit possible!');
+    //         await this.postComplete();
+    //       },
+    //     });
+    //   }
+    // }
   }
 
   public preventSubmitIncaseTheresNoBackendValidation() {
