3370dfccba28dd25f5322078f789c617c744df1c
Konstantin Vengerov <kvengerov@gmail.com>
2023-03-30 11:37:10 +0300

    [USH-298] Category filter (#187)
    * [USH-255] Category filter
    
    * [USH-255] Category filter
    
    * remove log
    
    * clear logs
    
    * increase debounce time
    
    ---------
    
    Co-authored-by: miraxes <mr.miraxes@gmail.com>

--- apps/web-mzima-client/src/app/map/map.component.ts
+++ apps/web-mzima-client/src/app/map/map.component.ts
@@ -123,7 +123,7 @@ export class MapComponent extends MainViewComponent implements OnInit {
   }
 
   private initFilterListener() {
-    this.filtersSubscription$.pipe(debounceTime(300)).subscribe({
+    this.filtersSubscription$.pipe(debounceTime(1000)).subscribe({
       next: () => {
         if (this.route.snapshot.data['view'] === 'search' && !this.searchId) return;
         if (this.route.snapshot.data['view'] === 'collection' && !this.collectionId) return;
@@ -133,7 +133,6 @@ export class MapComponent extends MainViewComponent implements OnInit {
           this.map.removeLayer(layer);
           this.markerClusterData.removeLayer(layer);
         });
-        console.log('sasda');
         this.mapLayers = [];
 
         this.getPostsGeoJson();
@@ -147,7 +146,6 @@ export class MapComponent extends MainViewComponent implements OnInit {
   }
 
   getPostsGeoJson() {
-    console.log('getPostsGeoJson');
     this.postsService.getGeojson(this.params).subscribe({
       next: (posts) => {
         const geoPosts = geoJSON(posts, {
