260d4f2a94aa05d6da6d48107aed870454df22d7
Konstantin Vengerov <kvengerov@gmail.com>
2023-03-31 14:51:06 +0300

    [USH-308] Post: Validation error message for entering certain special symbols (#194)
    * filters
    
    * Post: Validation error message for entering certain special symbols
    
    * Post: Validation error message for entering certain special symbols
    
    * Post: Validation error message for entering certain special symbols

--- apps/web-mzima-client/src/app/core/validators/alphanumeric.ts
+++ apps/web-mzima-client/src/app/core/validators/alphanumeric.ts
@@ -1,8 +1,8 @@
 import { AbstractControl, ValidatorFn } from '@angular/forms';
 
-export function excludeSpecialCharactersValidator(): ValidatorFn {
+export function AlphanumericValidatorValidator(): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
-    const pattern = /^[^#$%^&*ˆ+=]*$/;
+    const pattern = /^[\p{L}\p{N}\s\-".?!;,@]*$/gmu;
     const isValid = pattern.test(control.value);
     return !isValid ? { specialCharacters: { value: control.value } } : null;
   };
