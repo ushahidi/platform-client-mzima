e815dadc383f1e5c9ac4fb57e16fe641e82489e1
konstantin <kvengerov@gmail.com>
2023-05-17 11:47:25 +0300

    rename folder

--- apps/mobile-mzima-client/src/app/core/constants/walkthrough.ts
+++ apps/mobile-mzima-client/src/app/core/constants/walkthrough.ts
@@ -1,21 +1,21 @@
 export const WalkthroughSlider = [
   {
-    img: '/assets/images/walkthrow/slide_1.svg',
+    img: '/assets/images/walkthrough/slide_1.svg',
     title: 'Add a deployment',
     description: 'To start using the app, search for your deployment and add it to your account.',
   },
   {
-    img: '/assets/images/walkthrow/slide_2.svg',
+    img: '/assets/images/walkthrough/slide_2.svg',
     title: 'Start information sharing',
     description: 'You can add posts by yourself or search for them in the Map View or Data View.',
   },
   {
-    img: '/assets/images/walkthrow/slide_3.svg',
+    img: '/assets/images/walkthrough/slide_3.svg',
     title: 'Use filter and sorting',
     description: 'Find specific posts and quickly organise the data by any criteria you choose.',
   },
   {
-    img: '/assets/images/walkthrow/slide_4.svg',
+    img: '/assets/images/walkthrough/slide_4.svg',
     title: 'Gain offline data collection',
     description: 'Keep collecting and reporting crucial information, even without internet access!',
   },
