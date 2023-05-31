3ed8c29a9872d630b11fd9cf988d920f339115b8
Dmytro Fast <df@proverstka.com>
2023-05-31 12:23:27 +0300

    add reset onboarding button

--- apps/mobile-mzima-client/src/app/settings/settings.page.ts
+++ apps/mobile-mzima-client/src/app/settings/settings.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService, DeploymentService } from '@services';
+import { AuthService, DeploymentService, StorageService } from '@services';
+import { STORAGE_KEYS } from '../core/constants/storage-key';
 
 @Component({
   selector: 'app-settings',
@@ -14,11 +15,19 @@ export class SettingsPage {
     private router: Router,
     private authService: AuthService,
     private deploymentService: DeploymentService,
+    private storageService: StorageService,
   ) {}
 
   public logout(): void {
     this.authService.logout();
     this.deploymentService.removeDeployment();
     this.router.navigate(['/deployment']);
+  }
+
+  public resetOnboarding(): void {
+    this.storageService.deleteStorage(STORAGE_KEYS.INTRO_DONE);
+    this.authService.logout();
+    this.deploymentService.removeDeployment();
+    this.router.navigate(['/walkthrough']);
   }
 }
