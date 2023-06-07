89464ca2c8f422658a3a7bf55831af9e07a4672a
konstantin <kvengerov@gmail.com>
2023-06-07 15:23:16 +0300

    change backNavigation method

--- apps/web-mzima-client/src/app/post/post-edit/post-edit.component.ts
+++ apps/web-mzima-client/src/app/post/post-edit/post-edit.component.ts
@@ -516,11 +516,13 @@ export class PostEditComponent implements OnInit, OnChanges {
       complete: async () => {
         await this.postComplete();
         this.updated.emit();
-        if (!this.modalView) {
-          this.backNavigation();
-        }
+        if (this.checkRoutes('feed')) this.backNavigation();
       },
     });
+  }
+
+  private checkRoutes(path: string) {
+    return this.router.url.includes(path);
   }
 
   private createPost(postData: any) {
@@ -571,8 +573,7 @@ export class PostEditComponent implements OnInit, OnChanges {
       if (!confirmed) return;
     }
 
-    if (!this.postInput) {
-      this.backNavigation();
+    if (!this.postId) {
       this.eventBusService.next({
         type: EventType.AddPostButtonSubmit,
         payload: true,
@@ -580,10 +581,22 @@ export class PostEditComponent implements OnInit, OnChanges {
     } else {
       this.cancel.emit();
     }
+    this.backNavigation();
   }
 
   public backNavigation(): void {
-    this.location.back();
+    let urlString = this.router.url;
+    if (this.checkRoutes('edit')) {
+      urlString = urlString.replace('edit', 'view');
+      const urlParts = urlString.split('?');
+      const url = urlParts[0];
+      let queryParams = {};
+      if (urlParts[1]) {
+        const params = new URLSearchParams(urlParts[1]);
+        queryParams = { mode: params.get('mode') };
+      }
+      this.router.navigate([url], { queryParams: queryParams });
+    }
   }
 
   public toggleAllSelection(event: MatCheckboxChange, fields: any, fieldKey: string) {
