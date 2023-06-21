1d412e83f9f4d8e023e569389309d02a01d0800c
MUGO BRIAN <mikanmuchiri@gmail.com>
2023-06-21 13:55:58 +0300

    Sidebar Images Accessibility: Added Accessibility features to the sidebar  (#330)
    * Added Accessibility features to the sidebar
    
    * Uses state params to change the ari-expand from  true to false
    
    * Remove all the unnecessary aria-expanded from the mat icons

--- apps/web-mzima-client/src/app/shared/components/company-info/company-info.component.ts
+++ apps/web-mzima-client/src/app/shared/components/company-info/company-info.component.ts
@@ -12,6 +12,8 @@ export class CompanyInfoComponent {
   public logo: string;
   public title: string;
   public description: string;
+  // Added to keep track of the arrow down icon in the expansion panel whether it is expanded or collapsed
+  public expanded: boolean = false;
 
   public isDescriptionOpen = true;
 
@@ -29,6 +31,8 @@ export class CompanyInfoComponent {
   }
 
   public descriptionToggleHandle(state: boolean) {
+    // Changes arrow down icon attr.aria-expanded to true or false
+    this.isDescriptionOpen = state;
     localStorage.setItem('is_description_open', JSON.stringify(state));
   }
 }
