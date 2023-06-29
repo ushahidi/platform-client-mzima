29c7de9ded365a203c74036300a123eee7417750
Dmytro Fast <df@proverstka.com>
2023-06-29 19:19:14 +0300

    Grammar fixes on mobile app

--- apps/mobile-mzima-client/src/app/core/constants/walkthrough.ts
+++ apps/mobile-mzima-client/src/app/core/constants/walkthrough.ts
@@ -6,17 +6,17 @@ export const WalkthroughSlider = [
   },
   {
     img: '/assets/images/walkthrough/slide_2.svg',
-    title: 'Start information sharing',
+    title: 'Start sharing information',
     description: 'You can add posts by yourself or search for them in the Map View or Data View.',
   },
   {
     img: '/assets/images/walkthrough/slide_3.svg',
-    title: 'Use filter and sorting',
+    title: 'Filter and sort your data',
     description: 'Find specific posts and quickly organise the data by any criteria you choose.',
   },
   {
     img: '/assets/images/walkthrough/slide_4.svg',
-    title: 'Gain offline data collection',
+    title: 'Collect your data offline',
     description: 'Keep collecting and reporting crucial information, even without internet access!',
   },
 ];
