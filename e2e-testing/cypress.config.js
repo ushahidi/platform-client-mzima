475e2d3daa73a8702999f9ff27352e29878cc68d
Walter Loso <w.obadha@gmail.com>
2023-06-30 05:09:59 +0300

    update project id

--- e2e-testing/cypress.config.js
+++ e2e-testing/cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  projectId: '43gftm',
+  projectId: "43gftm",
   viewportWidth: 1280,
   viewportHeight: 960,
   defaultCommandTimeout: 20000,
