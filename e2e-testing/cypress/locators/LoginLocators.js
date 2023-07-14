d396c4bcc4c3877cea080e39e1e61d6c2d2e5f2a
Walter <w.obadha@gmail.com>
2023-07-14 16:08:45 +0300

    add verification for login

--- e2e-testing/cypress/locators/LoginLocators.js
+++ e2e-testing/cypress/locators/LoginLocators.js
@@ -4,6 +4,7 @@ const LoginLocators = {
   passwordField: '[data-qa="password"]',
   loginButton: '[data-qa="btn-login"]',
   logoutButton: '[data-qa="btn-logout"]',
+  accountBtn: '[data-qa="account-info"]',
 };
 
 export default LoginLocators;
