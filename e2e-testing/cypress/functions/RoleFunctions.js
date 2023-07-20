137b93e891d9c4df9a85f9c1d7012b79d5ab01cd
AmTryingMyBest <38259840+AmTryingMyBest@users.noreply.github.com>
2023-07-20 14:28:18 +0300

    E2e adjust roles (#394)
    * change name of created role
    *  update created role selector
    * allow add role step to complete
    * enable videos to see what's going on
    * check url to allow role creation test to complete
    * separate role creation and deletion tests
    * adjust for timezones in test environment
    * change role to be deleted
    * change permission for role getting created
    * update role tests
    * chore: fix configuration of backend URL for e2e-test
    * E2e update surveys (#393)
    * update survey tests
    
    ---------
    
    Co-authored-by: David Losada Carballo <david@tuxpiper.com>

--- e2e-testing/cypress/functions/RoleFunctions.js
+++ e2e-testing/cypress/functions/RoleFunctions.js
@@ -10,30 +10,31 @@ class RoleFunctions {
     cy.get(RoleLocators.addRoleBtn).click();
     cy.get(RoleLocators.roleNameFld).type(roleName);
     cy.get(RoleLocators.roleDescFld).type(roleDescription);
-    cy.get(RoleLocators.manageUsersBox).click();
+    cy.get(RoleLocators.manageCollectionsBox).click();
     cy.get(RoleLocators.addSaveBtn).click();
   }
 
   verify_role_exist() {
-    cy.get(RoleLocators.createdRoleBtn).should("exist");
+    cy.get(RoleLocators.createdRoleBtn).should('exist');
   }
 
   delete_role() {
-    cy.get(RoleLocators.createdRoleBtn).click();
+    cy.get(RoleLocators.roleToDeleteBtn).click();
     cy.wait(1000);
     cy.get(RoleLocators.deleteRoleBtn).click();
     cy.get(RoleLocators.deleteConfirmBtn).click();
   }
 
   verify_role_deleted() {
-    cy.get(RoleLocators.createdRoleBtn).should("not.exist");
+    cy.get(RoleLocators.createdRoleBtn).should('not.exist');
   }
   add_and_verify_role() {
     this.open_roles_page();
-    this.add_role("Automated Role", "An automated description for automated role");
+    this.add_role('Automation Role 1.0', 'An automated description for automated role');
     this.verify_role_exist();
   }
   delete_role_and_verify_deletion() {
+    this.open_roles_page();
     this.delete_role();
     this.verify_role_deleted();
   }
