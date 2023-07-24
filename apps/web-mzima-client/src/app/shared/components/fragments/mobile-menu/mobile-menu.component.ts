05a3d281fc0aabf586a49ada3f9347a099e287b1
Ifycode <marychord@gmail.com>
2023-07-24 18:57:19 +0100

    Code clean up

--- apps/web-mzima-client/src/app/shared/components/fragments/mobile-menu/mobile-menu.component.ts
+++ apps/web-mzima-client/src/app/shared/components/fragments/mobile-menu/mobile-menu.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable, fromEvent } from 'rxjs';
-// import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy'; //- remove this?
 import { NavToolbarService } from '../../../services/shared.navtoolbar.service';
 
-//@UntilDestroy() //- remove this?
 @Component({
   selector: 'app-mobile-menu',
   templateUrl: './mobile-menu.component.html',
@@ -17,21 +15,9 @@ export class MobileMenuComponent {
   public clickObservable: Observable<Event> = fromEvent(document, 'click');
 
   constructor(private navToolbarService: NavToolbarService) {
-    this.isDesktop$ = this.navToolbarService.getScreenSize(); //.pipe(untilDestroyed(this)); //- remove this?
+    this.isDesktop$ = this.navToolbarService.getScreenSize();
     this.subscribeToclickObservable();
   }
-
-  // ngOnInit(): void {
-  //   // this.userData$.subscribe((userData) => {
-  //   //   this.profile = userData;
-  //   //   this.isLoggedIn = !!userData.userId;
-  //   //   this.isAdmin = userData.role === 'admin';
-  //   //   this.canRegister = !this.siteConfig.private && !this.siteConfig.disable_registration;
-  //   //   // this.initMenu();
-  //   // });
-  //   this.menu = this.navToolbarService.initMenu();
-  //   console.log(this.menu);
-  // }
 
   private subscribeToclickObservable() {
     this.clickObservable.subscribe(() => {
