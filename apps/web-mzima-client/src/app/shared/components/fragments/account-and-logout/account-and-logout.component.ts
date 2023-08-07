635dab2afb29a31dad9bd54278be63e85cab4564
Ifycode <marychord@gmail.com>
2023-08-07 02:46:12 +0100

    Extract the whole of Account settings and logout (desktop)

--- apps/web-mzima-client/src/app/shared/components/fragments/account-and-logout/account-and-logout.component.ts
+++ apps/web-mzima-client/src/app/shared/components/fragments/account-and-logout/account-and-logout.component.ts
@@ -1,63 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { UserMenuInterface } from '@models';
-import { UserInterface } from '@mzima-client/sdk';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { AuthService, SessionService } from '@services';
-import { Observable } from 'rxjs';
-import { AccountSettingsModalComponent } from '../../account-settings-modal/account-settings-modal.component';
+import { Component, OnInit } from '@angular/core';
 
-@UntilDestroy()
+import { AuthService, BreakpointService, SessionService } from '@services';
+import { NavToolbarService } from '../../../helpers/navtoolbar.service';
+import { BaseComponent } from '../../../../base.component';
+
 @Component({
   selector: 'app-account-and-logout',
   templateUrl: './account-and-logout.component.html',
   styleUrls: ['./account-and-logout.component.scss'],
 })
-export class AccountAndLogoutComponent implements OnInit {
-  @Input() isLink = false;
-
-  private userData$: Observable<UserInterface>;
-  public menu: UserMenuInterface[];
-  public isLoggedIn = false;
+export class AccountAndLogoutComponent extends BaseComponent implements OnInit {
   constructor(
-    private session: SessionService,
-    private dialog: MatDialog,
+    protected override sessionService: SessionService,
+    protected override breakpointService: BreakpointService,
     private authService: AuthService,
+    private navToolbarService: NavToolbarService,
   ) {
-    this.userData$ = this.session.currentUserData$.pipe(untilDestroyed(this));
+    super(sessionService, breakpointService);
+    this.checkDesktop();
   }
 
   ngOnInit(): void {
-    this.userData$.subscribe((userData) => {
-      this.isLoggedIn = !!userData.userId;
-      this.initMenu();
-    });
+    this.getUserData();
   }
 
-  private initMenu() {
-    this.menu = [
-      {
-        label: 'nav.my_account',
-        icon: 'account',
-        visible: this.isLoggedIn,
-        action: () => this.openSettings(),
-        separator: true,
-      },
-      {
-        label: 'nav.logout',
-        icon: 'logout',
-        visible: this.isLoggedIn,
-        action: () => this.logout(),
-      },
-    ];
-  }
+  loadData(): void {}
 
-  public openSettings(): void {
-    this.dialog.open(AccountSettingsModalComponent, {
-      width: '100%',
-      maxWidth: 800,
-      panelClass: ['modal', 'account-settings-modal'],
-    });
+  public openAccountSettings(): void {
+    this.navToolbarService.openAccountSettings();
   }
 
   public logout(): void {
