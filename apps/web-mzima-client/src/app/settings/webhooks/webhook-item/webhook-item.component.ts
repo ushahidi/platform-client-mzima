9182bcb3042862189d59f10acbab25f194dd25a1
Konstantin Vengerov <kvengerov@gmail.com>
2023-08-14 16:00:13 +0300

    [USH-715] Settings: Unable to save a webhook (#494)
    * [USH-715] Settings: Unable to save a webhook
    
    * Update Map Precision Level

--- apps/web-mzima-client/src/app/settings/webhooks/webhook-item/webhook-item.component.ts
+++ apps/web-mzima-client/src/app/settings/webhooks/webhook-item/webhook-item.component.ts
@@ -2,7 +2,7 @@ import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { takeUntilDestroy$ } from '@helpers';
+import { regexHelper, takeUntilDestroy$ } from '@helpers';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
 import { BreakpointService } from '@services';
@@ -64,19 +64,13 @@ export class WebhookItemComponent implements OnInit {
       destination_field_key: [null],
       entity_type: ['', [Validators.required]],
       event_type: ['', [Validators.required]],
-      form_id: [0],
+      form_id: [null],
       id: [0],
       name: ['', [Validators.required]],
       shared_secret: ['', [Validators.required, Validators.minLength(20)]],
       source_field_key: [null],
       updated: [''],
-      url: [
-        '',
-        [
-          Validators.required,
-          Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'),
-        ],
-      ],
+      url: ['', [Validators.required, Validators.pattern(regexHelper.urlValidate())]],
       user: [''],
       webhook_uuid: [''],
       is_source_destination: [false],
@@ -193,7 +187,6 @@ export class WebhookItemComponent implements OnInit {
 
   private postWebhook() {
     this.deleteFormFields(['id', 'created', 'updated', 'allowed_privileges', 'user']);
-    if (this.form.controls['form_id'].value) this.deleteFormFields(['form_id']);
     this.webhooksService.post(this.form.value).subscribe({
       next: () => this.navigateToWebhooks(),
       error: (err) => {
