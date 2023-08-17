ddee503b3e4c621d0f2249dd2f5af7f5c85e70c4
Dmytro Fast <df@proverstka.com>
2023-08-17 20:02:06 +0300

    Update media interface

--- libs/sdk/src/lib/models/media.interface.ts
+++ libs/sdk/src/lib/models/media.interface.ts
@@ -5,10 +5,15 @@ export interface MediaResponse extends ApiResponse {
 }
 
 export interface MediaResult {
-  allowed_privileges: string[];
-  description: string;
-  id: number;
-  name: string;
-  url: string;
+  id: 1012;
+  user_id: number | null;
   caption?: string;
+  mime?: string;
+  original_file_url: string;
+  original_file_size: number;
+  original_width: number;
+  original_height: number;
+  created: Date;
+  updated?: Date | null;
+  allowed_privileges: string[];
 }
