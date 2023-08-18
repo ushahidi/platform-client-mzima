46c987a976969d512f8c063aaf7fad0d5321c20c
Fast Dmytro <38496576+DmitryFast@users.noreply.github.com>
2023-08-18 13:11:46 +0300

    When the first picture field is empty, the next added picture in order is not displayed. (#515)

--- apps/web-mzima-client/src/app/post/post-preview/post-preview.component.ts
+++ apps/web-mzima-client/src/app/post/post-preview/post-preview.component.ts
@@ -45,7 +45,7 @@ export class PostPreviewComponent implements OnInit, OnChanges {
       this.allowed_privileges = this.post?.allowed_privileges ?? '';
       const mediaField = this.post.post_content?.flatMap((post_content) => {
         return post_content.fields.filter((field) => {
-          return field.type === 'media';
+          return field.type === 'media' && field.value?.id;
         });
       })[0];
       if (mediaField?.value?.id) {
