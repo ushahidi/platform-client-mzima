ffad34c8adea15864c1415812f81b99723a61816
miraxes <mr.miraxes@gmail.com>
2023-09-11 11:04:19 +0300

    Retry config on 401 on init

--- apps/mobile-mzima-client/src/app/core/services/config.service.ts
+++ apps/mobile-mzima-client/src/app/core/services/config.service.ts
@@ -78,7 +78,12 @@ export class ConfigService {
 
   initAllConfigurations(): Promise<any> {
     if (navigator.onLine) {
-      return lastValueFrom(this.getConfig());
+      return lastValueFrom(this.getConfig()).then(
+        () => {},
+        () => {
+          return lastValueFrom(this.getConfig());
+        },
+      );
     } else {
       return lastValueFrom(
         forkJoin([
