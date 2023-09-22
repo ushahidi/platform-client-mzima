85d69c80f17d4611b6f2e3949859fb9a216043d7
Ifycode <marychord@gmail.com>
2023-09-22 14:36:54 +0100

    Initial: Add post not found image to details component

--- apps/web-mzima-client/src/app/post/index.ts
+++ apps/web-mzima-client/src/app/post/index.ts
@@ -2,3 +2,4 @@ export { PostEditComponent } from './post-edit/post-edit.component';
 export { AddPostModalComponent } from './add-post-modal/add-post-modal.component';
 export { LocationSelectComponent } from './location-select/location-select.component';
 export { PostDetailsComponent } from './post-details/post-details.component';
+export { PostNotFoundComponent } from './post-not-found/post-not-found.component';
