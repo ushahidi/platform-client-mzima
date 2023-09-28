7f8707d23b2e9d1485a063fda798bf52c6bb28d8
miraxes <mr.miraxes@gmail.com>
2023-09-28 12:31:37 +0300

    Data import validation

--- apps/web-mzima-client/src/app/settings/data-import/data-import.component.ts
+++ apps/web-mzima-client/src/app/settings/data-import/data-import.component.ts
@@ -16,6 +16,7 @@ import { PollingService } from '../../core/services/polling.service';
 import { LoaderService } from '../../core/services/loader.service';
 import { ConfirmModalService } from '../../core/services/confirm-modal.service';
 import { BreakpointService, SessionService } from '@services';
+import _ from 'lodash';
 
 enum PostStatus {
   Published = 'published',
@@ -75,6 +76,40 @@ export class DataImportComponent extends BaseComponent implements OnInit {
     reader.readAsDataURL($event.target.files[0]);
   }
 
+  transformAttributes(attributes: any) {
+    const titleAttr = _.find(attributes, { type: 'title' });
+    const descAttr = _.find(attributes, { type: 'description' });
+    const titleLabel = titleAttr
+      ? titleAttr.label
+      : this.translateService.instant('post.modify.form.title');
+    const descLabel = descAttr
+      ? descAttr.label
+      : this.translateService.instant('post.modify.form.description');
+
+    attributes = _.chain(attributes)
+      .reject({ type: 'point' })
+      .reject({ type: 'title' })
+      .reject({ type: 'description' })
+      .push(
+        {
+          key: 'title',
+          label: titleLabel,
+          priority: 0,
+          required: true,
+        },
+        {
+          key: 'content',
+          label: descLabel,
+          priority: 1,
+          required: true,
+        },
+      )
+      .sortBy('priority')
+      .value();
+
+    return attributes;
+  }
+
   private checkFormAndFile() {
     if (this.selectedFile && this.selectedForm) {
       this.loader.show();
@@ -94,9 +129,9 @@ export class DataImportComponent extends BaseComponent implements OnInit {
             next: (result) => {
               this.loader.hide();
               this.selectedForm.tasks = result[0];
-              this.selectedForm.attributes = result[1];
+              this.selectedForm.attributes = this.transformAttributes(result[1]);
               this.hasRequiredTask = this.selectedForm.tasks.some((task) => task.required);
-              this.setRequiredFields(this.selectedForm.attributes);
+              this.setRequiredFields(this.selectedForm.attributes!);
             },
           });
           this.uploadErrors = [];
@@ -117,6 +152,7 @@ export class DataImportComponent extends BaseComponent implements OnInit {
   }
 
   setRequiredFields(attributes: FormAttributeInterface[]) {
+    this.requiredFields.clear();
     attributes.forEach((attr) => {
       if (attr.required) {
         this.requiredFields.set(attr.key, attr.label);
@@ -153,8 +189,79 @@ export class DataImportComponent extends BaseComponent implements OnInit {
     return map;
   }
 
+  validateCSV() {
+    const csvIsValid = true;
+    if (_.every(this.uploadedCSV.maps_to, _.isEmpty)) {
+      this.notification.showError(this.translateService.instant('notify.data_import.no_mappings'));
+      return false;
+    }
+
+    const duplicateVars = this.checkForDuplicates();
+    const allDuplicatesAreEmpty =
+      duplicateVars.filter((o) => o === '').length === duplicateVars.length;
+    // if duplicate var only holds '' , warn that column names cannot be empty
+    if (duplicateVars.length > 0 && !allDuplicatesAreEmpty) {
+      this.notification.showError(
+        this.translateService.instant('notify.data_import.duplicate_fields', {
+          duplicates: duplicateVars.join(', '),
+        }),
+      );
+      return false;
+    } else if (duplicateVars.length > 0) {
+      // if duplicate var only holds '' , warn that column names cannot be empty
+      this.notification.showError(
+        this.translateService.instant('notify.data_import.empty_mapping_empty'),
+      );
+      return false;
+    }
+
+    //Check required fields are set
+    const missing = this.checkRequiredFields(this.maps_to);
+    if (!_.isEmpty(missing)) {
+      this.notification.showError(
+        this.translateService.instant('notify.data_import.required_fields', {
+          required: missing.join(', '),
+        }),
+      );
+      return false;
+    }
+    return csvIsValid;
+  }
+
+  checkForDuplicates() {
+    // Check to make sure the user hasn't double mapped a key
+    // First, collect the counts for all keys
+    // Remove empty fields
+    const map = _.omitBy(this.maps_to, _.isNil);
+    return _.chain(map)
+      .map((item) => {
+        return this.uploadedCSV.columns[item];
+      })
+      .countBy((item) => {
+        return item;
+      })
+      .pickBy((v) => {
+        return v > 1;
+      })
+      .keys()
+      .value();
+  }
+
+  checkRequiredFields(fields: any) {
+    const missing: any = [];
+    this.requiredFields.forEach((v, k) => {
+      if (_.isNil(fields[k])) {
+        missing.push(v);
+      }
+    });
+    return missing;
+  }
+
   finish() {
     this.uploadedCSV.maps_to = this.remapColumns();
+    if (!this.validateCSV()) {
+      return;
+    }
     this.uploadedCSV.fixed = { form: this.selectedForm.id };
 
     if (this.statusOption === 'mark_as') {
