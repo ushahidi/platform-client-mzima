5aa396acc079cfaeb250ef59f1ba7ffe286cd584
oyenuga17 <64274826+oyenuga17@users.noreply.github.com>
2023-10-05 23:38:10 +0100

    Create CategoryLocators.js

---
+++ e2e-testing/cypress/locators/CategoryLocators.js
@@ -0,0 +1,10 @@
+const CategoryLocators = {
+  stngsBtn: '[data-qa="btn-settings"]',
+  ctgryBtn: '[data-qa="btn-categories"]',
+  addCategoryBtn: '[data-qa="btn-settings-create"]',
+  ctgryNameField: '[data-qa="name"]',
+  ctgryDescField: '[data-placeholder="Enter description..."]',
+  saveCtgryBtn: '[data-qa="btn-category-save"]',
+};
+
+export default CategoryLocators;
