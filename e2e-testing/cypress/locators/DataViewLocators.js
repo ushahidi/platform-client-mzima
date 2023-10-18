0fd4f25ee6e4bb1321602d4290146d8841ee3bb2
oyenuga17 <64274826+oyenuga17@users.noreply.github.com>
2023-10-18 09:20:31 +0100

    add data view locators

---
+++ e2e-testing/cypress/locators/DataViewLocators.js
@@ -0,0 +1,12 @@
+const DataViewLocators = {
+    srvyItemBtn: '[data-qa="add-post-modal-surveys-item649"]',
+    successButton: '[data-qa="btn-confirm-success"]',
+    submitBtn: '[data-qa="btn-post-item-submit"]',
+    dataViewBtn: '[data-qa="btn-data"]',
+  
+    titleField: '[data-qa="title"]',
+    descField: '[data-qa="description"]',
+  };
+  
+  export default DataViewLocators;
+  
