42f27f6d6416c32d3c4e7c77f390ef113213eb07
Caleno-R <calenorose@gmail.com>
2023-10-19 20:58:26 +0300

    feat(cypress-functions): create the general setting class
    create the general setting class containing modularized cypress
    functions for step by step testing

---
+++ e2e-testing/cypress/functions/GeneralSettingsFunction.js
@@ -0,0 +1,41 @@
+import GeneralSettingsLocator from "../locators/GeneralSettingsLocator";
+
+class GeneralSettingsFunctions{
+    // navigate to general settings
+    open_general_settings_page(){
+        cy.get(GeneralSettingsLocator.settingsBtn).click();
+    }
+
+    // edit deployment name
+    type_deployment_name(deploymentName){
+        cy.wait(1000);
+        cy.get(GeneralSettingsLocator.deploymentNameField).clear().type(deploymentName).should('have.value', deploymentName);
+
+    }
+
+    // edit description
+    type_site_description(description){
+        cy.wait(1000);
+        cy.get(GeneralSettingsLocator.descriptionField).clear().type(description).should('have.value', description);
+    }
+
+    // click save button
+    click_save_button(){
+        cy.get(GeneralSettingsLocator.saveButton).click();
+    }
+
+    // Api Key value
+    get_api_key_field_value(){
+        cy.wait(1000);
+        cy.get(GeneralSettingsLocator.apiKeyField).should('not.be.empty');
+    }
+
+    // tests
+    edit_general_page(){
+        this.type_deployment_name("Monovista");
+        this.type_site_description("Fixtures are a great way to mock data for responses to routes");
+        this.click_save_button()
+    }
+}
+
+export default GeneralSettingsFunctions;
