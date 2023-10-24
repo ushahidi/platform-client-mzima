8201ee20fd4a650af4d67ffe2f082ee58a5595c8
System Administrator <root@Shakiras-MacBook-Pro.local>
2023-10-24 04:59:20 +0300

    Refactor code tohave custom selectors

--- e2e-testing/cypress/functions/SurveyFilterFunctions.js
+++ e2e-testing/cypress/functions/SurveyFilterFunctions.js
@@ -5,14 +5,14 @@ class SurveyFilterFunctions {
     }
 
     check_post_filter_by_survey(){
-        cy.get('.feed-page__main')
+        cy.get(SurveyFilterLocators.posts)
         .contains('There are no posts yet!').should('exist');
-        cy.get('mat-selection-list[name="surveys"]')
-        .children('mat-list-option')
+        cy.get(SurveyFilterLocators.surveySelectionList)
+        .children(SurveyFilterLocators.surveySelectItem)
         .eq(0)
         .click({force: true})
-        cy.get('ngx-masonry')
-        .children('app-post-preview')
+        cy.get(SurveyFilterLocators.postPreview)
+        .children(SurveyFilterLocators.postItem)
         .should('not.be.empty');
 
     }
--- e2e-testing/cypress/locators/SurveyFilterLocators.js
+++ e2e-testing/cypress/locators/SurveyFilterLocators.js
@@ -1,5 +1,10 @@
 const SurveyFilterLocators = {
     dataViewBtn: '[data-qa="btn-data"]',
+    posts: '[data-qa="posts"]',
+    surveySelectionList: '[data-qa="survey-selection-list"]',
+    surveySelectItem: '[data-qa="survey-select-item"]',
+    postPreview: '[data-qa="post-preview"]',
+    postItem: '[data-qa="post-item"]'
 };
 
 export default SurveyFilterLocators;
