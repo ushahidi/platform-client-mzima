d89eec20d1406fb84dc369753c0406b0f285450f
MugoBrian <mikanmuchiri@gmail.com>
2023-10-26 11:20:07 +0300

    menu-non-links to menu-modal-popups spec .ts file

--- apps/web-mzima-client/src/app/shared/components/fragments/menu-modal-popups/menu-modal-popups.component.spec.ts
+++ apps/web-mzima-client/src/app/shared/components/fragments/menu-modal-popups/menu-modal-popups.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MenuListNonLinksComponent } from './menu-list-non-links.component';
+import { MenuModalPopupsComponent } from './menu-modal-popups.component';
 
-describe('MenuListNonLinksComponent', () => {
-  let component: MenuListNonLinksComponent;
-  let fixture: ComponentFixture<MenuListNonLinksComponent>;
+describe('MenuModalPopupsComponentt', () => {
+  let component: MenuModalPopupsComponent;
+  let fixture: ComponentFixture<MenuModalPopupsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [MenuListNonLinksComponent],
+      declarations: [MenuModalPopupsComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(MenuListNonLinksComponent);
+    fixture = TestBed.createComponent(MenuModalPopupsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
