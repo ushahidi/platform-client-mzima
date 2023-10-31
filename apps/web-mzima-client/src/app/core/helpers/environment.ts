175bf747d3d62d8f7f1bbf393be66b8eaba8258e
David Losada Carballo <david@tuxpiper.com>
2023-10-31 15:55:40 +0100

    fix: allow relative URLs in backend_url, add other checks for valid URLs

--- apps/web-mzima-client/src/app/core/helpers/environment.ts
+++ apps/web-mzima-client/src/app/core/helpers/environment.ts
@@ -1,11 +1,28 @@
 export const checkBackendURL = (backendUrl: string) => {
-  if (backendUrl.indexOf('http') === -1) {
-    backendUrl = `https://${backendUrl}`;
+  let parseURL = (url: string) => {
+    try {
+      return new URL(url);
+    } catch (e) {
+      // Attempt to build based on the current document
+      if (e instanceof TypeError) {
+        return new URL(url, document.baseURI);
+      } else {
+        throw e;
+      }
+    }
+  };
+  
+  let result = parseURL(backendUrl);
+
+  // Query string or hash not allowed
+  if ((result.search !== '') || (result.hash !== '')) {
+    throw new Error('Invalid backend URL');
   }
 
-  if (backendUrl.slice(-1) !== '/') {
-    backendUrl = `${backendUrl}/`;
+  // Ensure the path ends in / 
+  if (result.pathname.slice(-1) !== '/') {
+    result.pathname += '/';
   }
 
-  return backendUrl;
+  return result.href;
 };
