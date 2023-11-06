d1e0e1487cc664a80a3bd64fc84060a1120fbe88
Walter <w.obadha@gmail.com>
2023-11-06 19:13:23 +0300

    only edit name field without clearing

--- e2e-testing/cypress/functions/GeneralSettingsFunctions.js
+++ e2e-testing/cypress/functions/GeneralSettingsFunctions.js
@@ -1,45 +1,50 @@
 import GeneralSettingsLocator from "../locators/GeneralSettingsLocator";
 
-class GeneralSettingsFunctions{
-    // navigate to general settings
-    open_general_settings_page(){
-        cy.get(GeneralSettingsLocator.settingsBtn).click();
-    }
+class GeneralSettingsFunctions {
+  // navigate to general settings
+  open_general_settings_page() {
+    cy.get(GeneralSettingsLocator.settingsBtn).click();
+  }
 
-    // edit deployment name
-    type_deployment_name(deploymentName){
-        cy.wait(1000);
-        cy.get(GeneralSettingsLocator.deploymentNameField).clear().type(deploymentName).should('have.value', deploymentName);
+  // edit deployment name
+  type_deployment_name(deploymentName) {
+    // cy.wait(1000);
+    cy.get(GeneralSettingsLocator.deploymentNameField)
+      .type(deploymentName)
+      .should(($input) => {
+        const val = $input.val();
+        expect(val).to.include(deploymentName);
+      });
+  }
 
-    }
+  // edit description
+  type_site_description(description) {
+    cy.wait(5000);
+    cy.get(GeneralSettingsLocator.descriptionField)
+      .clear()
+      .type(description)
+      .should('have.value', description);
+  }
 
-    // edit description
-    type_site_description(description){
-        cy.wait(5000);
-        cy.get(GeneralSettingsLocator.descriptionField).clear().type(description).should('have.value', description);
-    }
+  // click save button
+  click_save_button() {
+    cy.wait(1000);
+    cy.get(GeneralSettingsLocator.saveButton).click();
+  }
 
-    // click save button
-    click_save_button(){
-        cy.wait(1000);
-        cy.get(GeneralSettingsLocator.saveButton).click();
-    }
+  // Api Key value
+  get_api_key_field_value() {
+    cy.wait(1000);
+    cy.get(GeneralSettingsLocator.apiKeyField).should('not.be.empty');
+  }
 
-    // Api Key value
-    get_api_key_field_value(){
-        cy.wait(1000);
-        cy.get(GeneralSettingsLocator.apiKeyField).should('not.be.empty');
-    }
-
-
-
-    // tests
-    edit_general_page(){
-        this.type_deployment_name("-Automated");
-        this.click_save_button();
-        this.type_site_description("Fixtures are a great way to mock data for responses to routes");
-        this.click_save_button();
-    }
+  // tests
+  edit_general_page() {
+    this.type_deployment_name('-Automated');
+    this.click_save_button();
+    this.type_site_description('Fixtures are a great way to mock data for responses to routes');
+    this.click_save_button();
+  }
 }
 
 export default GeneralSettingsFunctions;
