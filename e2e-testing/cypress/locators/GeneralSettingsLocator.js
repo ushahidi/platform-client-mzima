5916c6701bc82f3080dd156f38bf0f7269517102
AmTryingMyBest <38259840+AmTryingMyBest@users.noreply.github.com>
2023-11-06 22:24:17 +0300

    minor adjustments to general settings page (#692)
    * feat(cypress-locators): create general settings locators
    
    create general settings locators for selected elements to be used in
    testing
    
    * feat(cypress-functions): create the general setting class
    
    create the general setting class containing modularized cypress
    functions for step by step testing
    
    * feat(e2e-test): create the main general file to test general page
    
    create the main general file to test the general settings page
    
    * feat(component-html): add the data-qa attributes to elements
    
    add the data-qa attributes to general settings page elements used in
    cypress testing
    
    * feat(package-lock): changes to the autoprefixer module
    
    * feat(cypress-functions): add the test for description field
    
    * feat(e2e-test): add test for deployment name and dscription field
    
    * rename GeneralSettingsFunctions to match naming convention
    
    * only edit name field without clearing
    
    * remove waits
    
    ---------
    
    Co-authored-by: Rose Caleno <59660721+Caleno-R@users.noreply.github.com>
    Co-authored-by: Caleno-R <calenorose@gmail.com>

---
+++ e2e-testing/cypress/locators/GeneralSettingsLocator.js
@@ -0,0 +1,12 @@
+const GeneralSettingsLocator = {
+    deploymentNameField: '[data-qa="name"]',
+    descriptionField: '[data-qa="description"]',
+    apiKeyField: '[data-qa="api_key"]',
+    saveButton: '[data-qa="btn-save"]',
+    cancelButton: '[data-qa="btn-cancel"]',
+    apiKeyCopyButton: '[data-qa="api-key-copy"]',
+    settingsBtn: '[data-qa="btn-settings"]',
+  };
+  
+  export default GeneralSettingsLocator;
+  
