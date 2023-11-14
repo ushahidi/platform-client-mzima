3cacfc33482f340922850adcecc32dbbabe60e32
oyenuga17 <64274826+oyenuga17@users.noreply.github.com>
2023-11-14 18:44:00 +0100

    restructure functions

--- e2e-testing/cypress/functions/SignupFunctions.js
+++ e2e-testing/cypress/functions/SignupFunctions.js
@@ -3,22 +3,27 @@ import LoginFunctions from './LoginFunctions';
 
 const loginFunctions = new LoginFunctions();
 
+function backspace(number) {
+  let str = '';
+  for (let i = 0; i < number; i++) {
+    str += '{backspace}';
+  }
+  return str;
+}
 class SignupFunctions {
   constructor() {
     this.uniqueName = `Cypress User${Math.floor(Math.random() * 900) + 100}`;
     this.uniqueEmail = `cypress.${Math.floor(Math.random() * 900) + 100}@xyz.com`;
   }
+  navigate_to_signup_modal() {
+    loginFunctions.launch_login_modal(Cypress.env('baseUrl'));
+    cy.wait(1000);
+    cy.get(SignupLocators.signupTab).eq(1).click();
+  }
+
   type_name(name) {
     cy.wait(1000);
     cy.get(SignupLocators.nameField).type(name).should('have.value', name);
-  }
-
-  verify_signed_user() {
-    cy.get(SignupLocators.accountInfoBtn).click();
-    cy.get(SignupLocators.accountStnsBtn).click();
-    cy.get(SignupLocators.displayNameField).should('have.value', this.uniqueName);
-    cy.get(SignupLocators.emailField).should('have.value', this.uniqueEmail);
-    cy.get(SignupLocators.closeDialogBtn).click();
   }
 
   fill_all_fields() {
@@ -26,6 +31,32 @@ class SignupFunctions {
     loginFunctions.type_email(this.uniqueEmail);
     loginFunctions.type_password('Password@Cypress2023');
     cy.get(SignupLocators.agreeToTerms).click({ force: true });
+  }
+
+  verify_signup_is_enabled() {
+    cy.get(SignupLocators.signupBtn).should('be.enabled');
+  }
+
+  verify_signup_is_disabled() {
+    cy.get(SignupLocators.signupBtn).should('be.disabled');
+  }
+
+  empty_name_field() {
+    cy.get(SignupLocators.nameField).clear();
+  }
+
+  alter_email_field() {
+    // delete xyz.com
+    cy.get(SignupLocators.emailField).type(backspace(7));
+    cy.get(SignupLocators.emailField).blur();
+  }
+
+  verify_email_is_invalid() {
+    cy.get(SignupLocators.invalidEmailError).should('be.visible');
+  }
+
+  restore_correct_email_value() {
+    cy.get(SignupLocators.emailField).type('xyz.com');
   }
 
   verify_privacy_and_terms_link() {
@@ -37,23 +68,33 @@ class SignupFunctions {
       .should('have.attr', 'href', 'https://www.ushahidi.com/terms-of-service');
   }
 
-  signup() {
-    loginFunctions.launch_login_modal(Cypress.env('baseUrl'));
-    cy.wait(1000);
-    cy.get(SignupLocators.signupTab).eq(1).click();
-    this.fill_all_fields();
-    cy.get(SignupLocators.signupBtn).should('be.enabled');
-    cy.get(SignupLocators.nameField).clear();
-    cy.get(SignupLocators.signupBtn).should('be.disabled');
-    cy.get(SignupLocators.emailField).type('{backspace}{backspace}{backspace}');
-    cy.get(SignupLocators.emailField).blur();
-    cy.get(SignupLocators.invalidEmailError).should('be.visible');
-    cy.get(SignupLocators.signupBtn).should('be.disabled');
-    this.type_name(this.uniqueName);
-    cy.get(SignupLocators.emailField).type('com');
-    cy.get(SignupLocators.signupBtn).should('be.enabled');
-    this.verify_privacy_and_terms_link();
+  click_signup_button() {
     cy.get(SignupLocators.signupBtn).click();
+  }
+
+  verify_signed_user() {
+    cy.get(SignupLocators.accountInfoBtn).click();
+    cy.get(SignupLocators.accountStnsBtn).click();
+    cy.get(SignupLocators.displayNameField).should('have.value', this.uniqueName);
+    cy.get(SignupLocators.emailField).should('have.value', this.uniqueEmail);
+    cy.get(SignupLocators.closeDialogBtn).click();
+  }
+
+  signup() {
+    this.navigate_to_signup_modal();
+    this.fill_all_fields();
+    this.verify_signup_is_enabled();
+    this.empty_name_field();
+    this.verify_signup_is_disabled();
+    this.alter_email_field();
+    this.verify_email_is_invalid();
+    this.verify_signup_is_disabled();
+    this.type_name(this.uniqueName);
+    this.restore_correct_email_value();
+    this.verify_signup_is_enabled();
+    this.verify_privacy_and_terms_link();
+    this.click_signup_button();
+    this.verify_signed_user();
   }
 }
 
