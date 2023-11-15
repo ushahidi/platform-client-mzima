462aeb5d5cf766fba800d33c854aa1c2f882ca14
Shakira Ndagire Seruwagi <ndagire.shakira@gmail.com>
2023-11-15 12:11:18 +0300

    E2e tests for Unique selectors for Posts, EditPost, Collections, Categories (#691)
    * Implement unique selectors for the functionality
    
    * Implement unique selectors for the functionality
    
    * Remove unnecessary changes
    
    * Remove unnecessary changes
    
    * Add changes  to end to end tests for posts
    
    * Remove the unused import
    
    * Update the PR with latest changes from development
    
    ---------
    
    Co-authored-by: Shakira <shakira@Shakiras-MacBook-Pro.local>
    Co-authored-by: AmTryingMyBest <38259840+AmTryingMyBest@users.noreply.github.com>

--- e2e-testing/cypress/functions/PostFunctions.js
+++ e2e-testing/cypress/functions/PostFunctions.js
@@ -8,31 +8,28 @@ class PostFunctions {
     this.postDescription = 'Automated Description Response';
   }
   open_post_creation_form() {
-    cy.get('.sidebar__add-post-btn .submit-post-button .mzima-button--primary').click();
+    cy.get(PostLocators.addPostBtn).click();
     cy.get(PostLocators.srvyItemBtn).click();
   }
 
-  fill_required_form_fields() {
-    cy.contains('mat-label', 'Title')
-      .siblings('mat-form-field')
-      .find('input.mat-input-element')
-      .type(this.postTitle);
-    cy.get(PostLocators.descField).type(this.postDescription);
+  type_post_title(title) {
+    cy.get(PostLocators.titleField).eq(0).type(title).should('have.value', title);
+  }
 
+  type_post_description(description) {
+    cy.get(PostLocators.descField).type(description).should('have.value', description);
+  }
+
+  fill_required_form_fields() {
+    this.type_post_title(this.postTitle);
+    this.type_post_description(this.postDescription);
     cy.get(PostLocators.lngTextField).type('This is an automated long text response');
-    cy.contains('mat-label', 'Number (Decimal) Field')
-      .siblings('mat-form-field')
-      .find('input.mat-input-element')
-      .type(99.9);
-    cy.contains('mat-label', 'Number (Integer) Field')
-      .siblings('mat-form-field')
-      .find('input.mat-input-element')
-      .type(100);
+    cy.get(PostLocators.shtTextField).eq(1).type('Automated Short text');
+    cy.get(PostLocators.decimalField).eq(2).type(99.9);
+    cy.get(PostLocators.intField).eq(4).type(100);
+
     // click on date field to open pop up
-    cy.contains('mat-label', 'Date & Time Field')
-      .siblings('mat-form-field')
-      .find('input.mat-input-element')
-      .click();
+    cy.get(PostLocators.dateField).eq(6).click();
     cy.get('.mat-button.mat-stroked-button').click(); // click on the checkmark button to select today's date
     cy.get(PostLocators.selectFieldBtn).click();
     cy.get(PostLocators.selectFieldOption1).click();
@@ -43,15 +40,17 @@ class PostFunctions {
   complete_add_post_steps() {
     cy.get(PostLocators.submitBtn).should('not.be.disabled');
     cy.get(PostLocators.submitBtn).click();
-    cy.get('div.confirm-content').should('be.visible');
     cy.get(PostLocators.successButton).click();
   }
 
   verify_created_post_exists() {
-    cy.contains('mat-list-option', "Full Length Survey-with image-field- don't delete")
-      .find('.mat-pseudo-checkbox')
-      .click();
-    cy.contains(this.postTitle).should('exist');
+    cy.get(PostLocators.surveySelectionList)
+        .children(PostLocators.surveySelectItem)
+        .contains("Full Length Survey-with image-field- don't delete")
+        .click({force: true})
+    cy.get(PostLocators.postPreview)
+      .children(PostLocators.postItem)
+      .contains(this.postTitle);
   }
 }
 
