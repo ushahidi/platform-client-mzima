2bdcef58a71bdb2b6e9da6601d083e60e245c5a1
Maxwell Mwandigha <maxmwandigha@gmail.com>
2023-11-16 15:14:57 +0300

    Updated the frontend code for image upload/delete feature

--- apps/mobile-mzima-client/src/app/profile/information/components/profile-photo/profile-photo.component.ts
+++ apps/mobile-mzima-client/src/app/profile/information/components/profile-photo/profile-photo.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { AlertService } from '@services';
+import { Component, Input } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { AlertService, SessionService } from '@services';
 
+@UntilDestroy()
 @Component({
   selector: 'app-profile-photo',
   templateUrl: './profile-photo.component.html',
@@ -8,15 +10,39 @@ import { AlertService } from '@services';
 })
 export class ProfilePhotoComponent {
   @Input() photo: string;
-  @Output() deletePhoto = new EventEmitter();
-  @Output() changePhoto = new EventEmitter();
+  // @Output() deletePhoto = new EventEmitter();
+  // @Output() changePhoto = new EventEmitter();
 
-  constructor(private alertService: AlertService) {}
+  constructor(private alertService: AlertService, private sessionService: SessionService) {}
 
-  public uploadNewPhoto(): void {
-    console.log('uploadNewPhoto');
-    this.changePhoto.emit();
+  selectPhoto(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const file = input?.files?.[0];
+    if (file) {
+      this.changePhoto(file);
+    }
   }
+
+  changePhoto(file: File): void {
+    const reader = new FileReader();
+    console.log(reader);
+    console.log(file);
+    reader.onload = () => {
+      this.photo = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  // My Proposed Code for API
+
+  // ngOnDestroy(): void {
+  //   this.saveImageOnUnload();
+  // }
+
+  // private saveImageOnUnload(): void {
+  //   //api to save image
+  //   console.log('Saving image', this.photo);
+  // }
 
   public async deletePhotoHandle(): Promise<void> {
     const result = await this.alertService.presentAlert({
@@ -36,7 +62,14 @@ export class ProfilePhotoComponent {
     });
 
     if (result.role === 'confirm') {
-      this.deletePhoto.emit();
+      this.sessionService
+        .getCurrentUserData()
+        .pipe(untilDestroyed(this))
+        .subscribe((userData) => {
+          this.photo = `https://www.gravatar.com/avatar/${
+            userData.gravatar || '00000000000000000000000000000000'
+          }?d=retro&s=256`;
+        });
     }
   }
 }
