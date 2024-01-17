6158fa8db999bda79ec252efe21764504ef469b5
Shakira <shakira@Shakiras-MacBook-Pro.local>
2024-01-17 08:49:58 +0300

    Add private deployments tests

---
+++ e2e-testing/cypress/locators/PrivateDeploymentLocators.js
@@ -0,0 +1,8 @@
+const GeneralSettingsLocator = {
+    settingsBtn: '[data-qa="btn-settings"]',
+    generalBtn: '[data-qa="btn-general"]',
+    privateCheckbox: '[data-qa="site-private"]',
+    saveBtn: '[data-qa="btn-save"]',
+    logoutBtn: '[data-qa="log-out"]',
+    
+  };
