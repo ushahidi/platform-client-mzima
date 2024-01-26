9cea20d5f66b168789f61aef2f08c7bbc43bb15c
AmTryingMyBest <38259840+AmTryingMyBest@users.noreply.github.com>
2024-01-26 21:05:04 +0300

    E2e login programatically using sessions (#752)
    * implement session within the login file
    
    * add step to open home page after verifying login in all files

--- e2e-testing/cypress/e2e/7-collections/collections.cy.js
+++ e2e-testing/cypress/e2e/7-collections/collections.cy.js
@@ -7,6 +7,7 @@ describe("Automated Tests for Collections", () => {
 
   beforeEach(() => {
     loginFunctions.login_as_admin();
+    cy.visit(Cypress.env('baseUrl'));
   });
 
   it("Opens Collections Modals", () => {
