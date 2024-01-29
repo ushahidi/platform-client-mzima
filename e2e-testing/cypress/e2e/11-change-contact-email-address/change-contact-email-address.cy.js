96440ae99ff32c04be816be7d6a3d12f3f158ed9
Shakira <shakira@Shakiras-MacBook-Pro.local>
2024-01-29 20:03:59 +0300

    fix the failing tests

--- e2e-testing/cypress/e2e/11-change-contact-email-address/change-contact-email-address.cy.js
+++ e2e-testing/cypress/e2e/11-change-contact-email-address/change-contact-email-address.cy.js
@@ -7,6 +7,7 @@ describe("Automated Tests for change contact email address", () => {
 
   beforeEach(() => {
     loginFunctions.login_as_admin();
+    cy.visit(Cypress.env('baseUrl'));
   });
 
   it("Steps to change contact email address", () => {
