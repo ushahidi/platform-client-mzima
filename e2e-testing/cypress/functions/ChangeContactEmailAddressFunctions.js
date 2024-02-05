d0982beae5cdec2a0da17fdb9be910a84fbe59cd
Shakira Ndagire Seruwagi <ndagire.shakira@gmail.com>
2024-02-05 14:28:03 +0300

    Add e2e tests to change contact email address (#745)
    * Add e2e tests to change contact email address
    
    * Add the remaining step on checklist
    
    * Rename the email locator to email field
    
    * Fix failing test
    
    * fix the failing tests
    
    ---------
    
    Co-authored-by: Shakira <shakira@Shakiras-MacBook-Pro.local>
    Co-authored-by: AmTryingMyBest <38259840+AmTryingMyBest@users.noreply.github.com>

---
+++ e2e-testing/cypress/functions/ChangeContactEmailAddressFunctions.js
@@ -0,0 +1,26 @@
+import ChangeContactEmailAddressLocators from '../locators/ChangeContactEmailAddressLocators';
+
+class ChangeContactEmailAddressFunctions {
+    open_general_settings() {
+      cy.get(ChangeContactEmailAddressLocators.settingsBtn).click(); 
+    }
+    change_contact_email_address(){
+        cy.get(ChangeContactEmailAddressLocators.emailField)
+        .clear()
+        .type("testnew@gmail.com")
+    }
+
+    click_save_button() {
+      cy.get(ChangeContactEmailAddressLocators.saveBtn).click();
+    }
+
+    check_new_contact_email_address_shows(){
+        cy.get(ChangeContactEmailAddressLocators.contactEmail).contains('testnew@gmail.com')
+    }
+
+    reload_page(){
+      cy.reload()
+    }
+  }
+
+  export default ChangeContactEmailAddressFunctions;
