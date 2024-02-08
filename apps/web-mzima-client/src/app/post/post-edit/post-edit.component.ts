309e1669be2332b3615fd78f86cc66c151471d3c
Anna Iosif <anna@ushahidi.com>
2024-02-08 16:47:51 +0100

    Adding child-categories to the view when adding/editing posts

--- apps/web-mzima-client/src/app/post/post-edit/post-edit.component.ts
+++ apps/web-mzima-client/src/app/post/post-edit/post-edit.component.ts
@@ -183,17 +183,6 @@ export class PostEditComponent extends BaseComponent implements OnInit, OnChange
     });
   }
 
-  getOrderedOptions(options: any[]) {
-    const result: any[] = [];
-    options
-      .filter((opt: any) => !opt.parent_id)
-      .forEach((parent) => {
-        result.push(parent);
-        result.push(...options.filter((opt: any) => opt.parent_id === parent.id));
-      });
-    return result;
-  }
-
   private loadSurveyData(formId: number | null, updateContent?: any[]) {
     if (!formId) return;
     this.surveysService.getSurveyById(formId).subscribe({
@@ -228,7 +217,6 @@ export class PostEditComponent extends BaseComponent implements OnInit, OnChange
                   this.description = field.default;
                   break;
                 case 'tags':
-                  field.options = this.getOrderedOptions(field.options);
                   this.description = field.default;
                   break;
                 case 'media': // Max image size addition hack
