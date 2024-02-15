9f6e20da447d7b6f9dcadea8581981e2a65f3376
Shakira Ndagire Seruwagi <ndagire.shakira@gmail.com>
2024-02-15 11:33:44 +0300

    e2e tests to verify-share-button-for-post (#777)
    Co-authored-by: Shakira <shakira@Shakiras-MacBook-Pro.local>

---
+++ e2e-testing/cypress/locators/PostsLocators/ShareButtonPostLocators.js
@@ -0,0 +1,13 @@
+const ShareButtonPostLocators = {
+    dataViewBtn: '[data-qa="btn-data"]',
+    surveySelectionList: '[data-qa="survey-selection-list"]',
+    surveySelectItem: '[data-qa="survey-select-item"]',
+    postPreview: '[data-qa="post-preview"]',
+    postItem: '[data-qa="post-item"]',
+    postHead: '[data-qa="post-head"]',
+    postShare: '[data-qa="share-post"]',
+    postDetails: '[data-qa="post-details"]',
+    surveyWebAddress: '[data-qa="survey-web-address"]',
+};
+
+export default ShareButtonPostLocators;
