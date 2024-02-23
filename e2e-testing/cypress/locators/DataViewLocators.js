35e2a002c5e0f4c20ceb7574397a51f92671f4da
AmTryingMyBest <w.obadha@gmail.com>
2024-02-23 19:03:21 +0300

    rename selectors and added comments for easier readability

--- e2e-testing/cypress/locators/DataViewLocators.js
+++ e2e-testing/cypress/locators/DataViewLocators.js
@@ -1,15 +1,15 @@
 const DataViewLocators = {
-    dataViewBtn: '[data-qa="btn-data"]',
-    posts: '[data-qa="posts"]',
-    surveySelectionList: '[data-qa="survey-selection-list"]',
-    surveySelectItem: '[data-qa="survey-select-item"]',
-    postPreview: '[data-qa="post-preview"]',
-    postItem: '[data-qa="post-item"]',
-    feedPageResults: '[data-qa="feed-page-results"]',
-    searchFormMainFiltersTotal: '[data-qa="search-form-main-filters-total"]',
-    clearBtn: '[data-qa="clear"]',
-    searchFormFiltersBtn: '[data-qa="search-form__filters"]',
-    searchFormButton: '[data-qa="search-form__button"]'
+  dataViewBtn: '[data-qa="btn-data"]',
+  posts: '[data-qa="posts"]',
+  surveySelectionList: '[data-qa="survey-selection-list"]',
+  surveySelectItem: '[data-qa="survey-select-item"]',
+  postPreview: '[data-qa="post-preview"]',
+  postItem: '[data-qa="post-item"]',
+  feedPageResults: '[data-qa="feed-page-results"]',
+  mainResultsTotal: '[data-qa="search-form-main-filters-total"]',
+  clearBtn: '[data-qa="clear"]',
+  revealFiltersBtn: '[data-qa="search-form__filters"]',
+  clearFiltersBtn: '[data-qa="search-form__button"]',
 };
 
 export default DataViewLocators;
