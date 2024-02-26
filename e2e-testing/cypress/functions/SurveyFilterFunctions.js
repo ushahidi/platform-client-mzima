71960e645f65f929f08efd8b24e87e9e564b237c
Shakira <shakira@Shakiras-MacBook-Pro.local>
2024-02-26 20:24:03 +0300

    Implement the changes to the e2e tests

--- e2e-testing/cypress/functions/SurveyFilterFunctions.js
+++ e2e-testing/cypress/functions/SurveyFilterFunctions.js
@@ -8,33 +8,6 @@ class SurveyFilterFunctions {
     cy.get(DataViewLocators.posts).contains('There are no posts yet!').should('exist');
     cy.get(DataViewLocators.postPreview).children(DataViewLocators.postItem).should('not.be.empty');
   }
-  check_post_filter_by_status(){
-    cy.get(DataViewLocators.searchFormFiltersBtn)
-      .click();
-    cy.get(DataViewLocators.feedPageResults)
-      // .contains("Current results: 20 / 513")
-      .contains("Current results: 2 / 2")
-    cy.get(DataViewLocators.statusBtn)
-    .click()
-    cy.get(DataViewLocators.filterSelectionList)
-      .find('.mat-list-item-content')
-      .eq(0)
-      .find('.mat-pseudo-checkbox')
-      .invoke('attr', 'class')
-      .should('contain', 'mat-pseudo-checkbox-checked')
-    cy.get(DataViewLocators.filterSelectionList)
-      .children(DataViewLocators.filterListOption)
-      .eq(1)
-      .find('.mat-pseudo-checkbox')
-      .invoke('attr', 'class')
-      .should('contain', 'mat-pseudo-checkbox-checked')
-    cy.get(DataViewLocators.filterSelectionList)
-      .children(DataViewLocators.filterListOption)
-      .eq(2)
-      .find('.mat-pseudo-checkbox')
-      .invoke('attr', 'class')
-      .should('not.contain', 'mat-pseudo-checkbox-checked')
-    }
 
   verify_count_on_results() {
     //verify results on landing on Data view
@@ -74,6 +47,105 @@ class SurveyFilterFunctions {
     cy.get(DataViewLocators.mainResultsTotal).contains('Results: 165');
     cy.get(DataViewLocators.feedPageResults).contains('Current results: 20 / 165');
   }
+
+  check_post_filter_by_status() {
+    //click search form filter button
+    cy.get(DataViewLocators.searchFormFiltersBtn)
+      .click();
+    cy.get(DataViewLocators.feedPageResults)
+      .contains("Current results: 2 / 2")
+    //click status filter button
+    cy.get(DataViewLocators.statusBtn)
+    .click()
+    //check that published option is checked
+    cy.get(DataViewLocators.filterSelectionList)
+      .find('.mat-list-item-content')
+      .eq(0)
+      .find('.mat-pseudo-checkbox')
+      .invoke('attr', 'class')
+      .should('contain', 'mat-pseudo-checkbox-checked')
+    //check that under review option is checked
+    cy.get(DataViewLocators.filterSelectionList)
+      .children(DataViewLocators.filterListOption)
+      .eq(1)
+      .find('.mat-pseudo-checkbox')
+      .invoke('attr', 'class')
+      .should('contain', 'mat-pseudo-checkbox-checked')
+    //check that archived option is unchecked
+    cy.get(DataViewLocators.filterSelectionList)
+      .children(DataViewLocators.filterListOption)
+      .eq(2)
+      .find('.mat-pseudo-checkbox')
+      .invoke('attr', 'class')
+      .should('not.contain', 'mat-pseudo-checkbox-checked')
+    //select the archived option  
+    cy.get(DataViewLocators.filterSelectionList)
+      .children(DataViewLocators.filterListOption)
+      .eq(2)
+      .find('.mat-pseudo-checkbox')
+      .click()
+    cy.get(DataViewLocators.feedPageResults)
+      .contains("Current results: 20 / 191");
+    //unselect archived and under review options
+    cy.get(DataViewLocators.filterSelectionList)
+      .children(DataViewLocators.filterListOption)
+      .eq(1)
+      .find('.mat-pseudo-checkbox')
+      .click()
+    cy.get(DataViewLocators.filterSelectionList)
+      .children(DataViewLocators.filterListOption)
+      .eq(2)
+      .find('.mat-pseudo-checkbox')
+      .click()
+    //check that only published is selected
+    cy.get(DataViewLocators.filterSelectionList)
+      .find('.mat-list-item-content')
+      .eq(0)
+      .find('.mat-pseudo-checkbox')
+      .invoke('attr', 'class')
+      .should('contain', 'mat-pseudo-checkbox-checked')
+    cy.get(DataViewLocators.feedPageResults)
+      .contains("Current results: 20 / 72")
+    //unselect published and select under review option
+    cy.get(DataViewLocators.filterSelectionList)
+      .children(DataViewLocators.filterListOption)
+      .eq(0)
+      .find('.mat-pseudo-checkbox')
+      .click()
+    cy.get(DataViewLocators.filterSelectionList)
+      .children(DataViewLocators.filterListOption)
+      .eq(1)
+      .find('.mat-pseudo-checkbox')
+      .click()
+    //check that only under review is selected
+    cy.get(DataViewLocators.filterSelectionList)
+      .children(DataViewLocators.filterListOption)
+      .eq(1)
+      .find('.mat-pseudo-checkbox')
+      .invoke('attr', 'class')
+      .should('contain', 'mat-pseudo-checkbox-checked')
+    cy.get(DataViewLocators.feedPageResults)
+      .contains("Current results: 20 / 104")
+    //clear all filters
+    cy.get(DataViewLocators.clearFiltersBtn)
+      .click()
+    //verify that published and under review are selected
+    cy.get(DataViewLocators.filterSelectionList)
+      .find('.mat-list-item-content')
+      .eq(0)
+      .find('.mat-pseudo-checkbox')
+      .invoke('attr', 'class')
+      .should('contain', 'mat-pseudo-checkbox-checked')
+    cy.get(DataViewLocators.filterSelectionList)
+      .children(DataViewLocators.filterListOption)
+      .eq(1)
+      .find('.mat-pseudo-checkbox')
+      .invoke('attr', 'class')
+      .should('contain', 'mat-pseudo-checkbox-checked')
+    cy.get(DataViewLocators.feedPageResults)
+      .contains("Current results: 2 / 2")
+    }
+
 }
 
 export default SurveyFilterFunctions;
