b2a3df7ddd8c13573d36b185ba6641ed66381143
Ifycode <marychord@gmail.com>
2024-03-01 09:54:01 +0100

    Fix added: Loader on load more click and indicate end of posts

--- apps/web-mzima-client/src/app/feed/feed.component.ts
+++ apps/web-mzima-client/src/app/feed/feed.component.ts
@@ -54,6 +54,7 @@ export class FeedComponent extends MainViewComponent implements OnInit {
   public posts: PostResult[] = [];
   public postCurrentLength = 0;
   public isLoading = false;
+  public loadingMorePosts: boolean;
   public mode: FeedMode = FeedMode.Tiles;
   public activePostId: number;
   public total: number;
@@ -304,6 +305,7 @@ export class FeedComponent extends MainViewComponent implements OnInit {
         });
         setTimeout(() => {
           this.isLoading = false;
+          this.loadingMorePosts = false; // for load more button's loader/spinner
           this.updateMasonry();
           setTimeout(() => {
             if (this.mode === FeedMode.Post && !isPostsAlreadyExist) {
@@ -511,8 +513,9 @@ export class FeedComponent extends MainViewComponent implements OnInit {
 
   public loadMore(): void {
     if (this.params.limit !== undefined && this.params.limit * this.params.page! < this.total) {
-      this.currentPage++;
-      this.params.page!++;
+      this.loadingMorePosts = true;
+      this.currentPage += 1;
+      this.params.page! += 1;
       this.getPostsSubject.next({ params: this.params });
     }
   }
