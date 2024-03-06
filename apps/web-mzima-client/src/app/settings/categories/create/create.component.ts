30b95999f73cdabfb94943fa5292e45530d5bee8
felabel <felicityabel2016@gmail.com>
2024-03-06 14:54:17 +0100

    handled error on creating categories

--- apps/web-mzima-client/src/app/settings/categories/create/create.component.ts
+++ apps/web-mzima-client/src/app/settings/categories/create/create.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CategoriesService } from '@mzima-client/sdk';
+import { NotificationService } from '@services';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-create',
@@ -10,17 +12,35 @@ import { CategoriesService } from '@mzima-client/sdk';
 export class CreateComponent {
   public isFormOnSubmit: boolean;
 
-  constructor(private categoriesService: CategoriesService, private router: Router) {}
+  constructor(
+    private categoriesService: CategoriesService,
+    private router: Router,
+    private notificationService: NotificationService,
+    private translate: TranslateService,
+  ) {}
 
   public createCategory(category: any): void {
     this.isFormOnSubmit = true;
 
     this.categoriesService.post(category).subscribe({
-      next: () => {
+      next: (response) => {
+        console.log('Backend response:', response);
+        if (response.OK) {
+          this.notificationService.showError(
+            this.translate.instant('An error occured, please try again'),
+          );
+        }
         this.isFormOnSubmit = false;
         this.router.navigate(['settings/categories']);
       },
       error: ({ error }) => {
+        console.log('Backend response:', error);
+        console.log('specific response:', error.errors[0].status);
+        if (error.errors[0].status === 404) {
+          this.notificationService.showError(
+            this.translate.instant('An error occured, please try again'),
+          );
+        }
         this.categoriesService.categoryErrors.next(error.errors.failed_validations);
         this.isFormOnSubmit = false;
       },
