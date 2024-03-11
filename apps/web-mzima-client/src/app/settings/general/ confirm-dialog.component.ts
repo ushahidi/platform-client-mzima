29519fb537a99782bcda6b915a769814a9a3af05
Rose Kivuva <rosekivuvan@gmail.com>
2024-03-11 19:49:39 +0300

    Updated my confirmation pop up

--- apps/web-mzima-client/src/app/settings/general/ confirm-dialog.component.ts
+++ apps/web-mzima-client/src/app/settings/general/ confirm-dialog.component.ts
@@ -3,30 +3,8 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirm-dialog',
-  template: `
-    <h2 mat-dialog-title style="font-size: 24px; font-weight: bold;">
-      The data has not been saved
-    </h2>
-    <div class="dialog-content" style="margin-bottom: 20px; font-size: 16px;">
-      This action cannot be undone. Please proceed with caution.
-    </div>
-    <div class="dialog-actions" style="display: flex; justify-content: flex-end;">
-      <button
-        class="dialog-button cancel-button"
-        style="background-color: white; color: #333; padding: 12px 24px;  width:25%; margin-right: 10px; border: 1px solid gray; border-radius: 3px; font-size: 14px; font-weight: semi-bold;"
-        (click)="onCancel()"
-      >
-        Cancel
-      </button>
-      <button
-        class="dialog-button confirm-button"
-        style="background-color: orange; color: #333; width: 25%; padding: 12px 24px; margin-left: 10px; border: none; border-radius: 3px; font-size: 14px; font-weight: semi-bold;"
-        (click)="onConfirm()"
-      >
-        OK
-      </button>
-    </div>
-  `,
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.scss']
 })
 export class ConfirmDialogComponent {
   constructor(
