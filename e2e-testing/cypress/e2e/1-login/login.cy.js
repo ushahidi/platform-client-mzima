41e92593c2b67cd50311edc4c6755988e43c36ae
aquilaafuadajo <aquilaafuadajo@gmail.com>
2024-03-12 12:47:04 +0100

    replace pa11y with cypress-axe

--- e2e-testing/cypress/e2e/1-login/login.cy.js
+++ e2e-testing/cypress/e2e/1-login/login.cy.js
@@ -1,9 +1,16 @@
 import LoginFunctions from "../../functions/LoginFunctions";
+// import terminalLog from "../../functions/accessibilityTerminalLog";
 
 describe("Login as Admin", () => {
   const loginFunctions = new LoginFunctions();
 
   it("Logs in as admin user", () => {
     loginFunctions.login_as_admin();
+  });
+
+
+  it("Should run accessibility audits", () => {
+    cy.injectAxe()
+    cy.checkA11y()
   });
 });
---
+++ e2e-testing/cypress/functions/AccessibilityTerminalLog.js
@@ -0,0 +1,19 @@
+function terminalLog(violations) {
+  cy.task(
+    'log',
+    `${violations.length} accessibility violation${violations.length === 1 ? '' : 's'} ${
+      violations.length === 1 ? 'was' : 'were'
+    } detected`,
+  );
+
+  const violationData = violations.map(({ id, impact, description, nodes }) => ({
+    id,
+    impact,
+    description,
+    nodes: nodes.length,
+  }));
+
+  cy.task('table', violationData);
+}
+
+export default terminalLog;
