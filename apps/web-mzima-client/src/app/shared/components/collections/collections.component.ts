6f170a864276a88dd23a82b772219888101e472b
claire256 <clairenabwami3@gmail.com>
2024-03-14 22:28:13 +0300

    Fix add collection with only me

--- apps/web-mzima-client/src/app/shared/components/collections/collections.component.ts
+++ apps/web-mzima-client/src/app/shared/components/collections/collections.component.ts
@@ -241,7 +241,9 @@ export class CollectionsComponent extends BaseComponent implements OnInit {
     const visibleTo = collectionData.visible_to.value;
     if (visibleTo === 'only_me') {
       collectionData.role = ['me'];
-      collectionData['view_only']['only_me'] = true;
+      collectionData['view_only'] = {
+        only_me: true
+      }
     } else if (visibleTo === 'everyone') {
       collectionData.role = ['everyone'];
     } else {
