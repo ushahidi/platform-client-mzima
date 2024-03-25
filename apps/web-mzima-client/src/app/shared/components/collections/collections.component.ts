5eb0a6226b06798857e074116eb15b9cacc88564
kivenalaric <elmozaro3@gmail.com>
2024-03-25 12:16:53 +0100

    removal of empty space

--- apps/web-mzima-client/src/app/shared/components/collections/collections.component.ts
+++ apps/web-mzima-client/src/app/shared/components/collections/collections.component.ts
@@ -244,7 +244,6 @@ export class CollectionsComponent extends BaseComponent implements OnInit {
       if (!collectionData['view_only']) {
         collectionData['view_only'] = {};
       }
-
       collectionData['view_only']['only_me'] = true;
     } else if (visibleTo === 'everyone') {
       collectionData.role = ['everyone'];
