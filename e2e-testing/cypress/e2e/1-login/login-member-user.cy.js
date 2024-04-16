5377282a3390c0efd60f57b8b2fabe96e31e867e
Walter <w.obadha@gmail.com>
2024-04-16 21:19:28 +0300

    re-organize login tests

--- e2e-testing/cypress/e2e/1-login/login-member-user.cy.js
+++
@@ -1,9 +0,0 @@
-import LoginFunctions from "../../functions/LoginFunctions";
-
-describe("Login as Member User", () => {
-  const loginFunctions = new LoginFunctions();
-
-  it("Logs in member user", () => {
-    loginFunctions.login_member_user();
-  });
-});
