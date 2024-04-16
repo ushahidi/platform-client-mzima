a3905a32630571fc03ec3549be2973c46b234fa1
Shakira <shakira@Shakiras-MacBook-Pro.local>
2024-04-16 10:36:27 +0300

    Implement feedback

--- e2e-testing/cypress/functions/CollectionFunctions.js
+++ e2e-testing/cypress/functions/CollectionFunctions.js
@@ -52,9 +52,7 @@ class CollectionFunctions {
     cy.get(CollectionLocators.successBtn).click();
     cy.get(CollectionLocators.postItem).eq(0).find(CollectionLocators.morePostActionsBtn).click();
     cy.get(CollectionLocators.addCollectionToPostCheckbox).click();
-    cy.get(CollectionLocators.selectCollection)
-      .find('#mat-checkbox-146-input')
-      .click({ force: true });
+    cy.get(CollectionLocators.selectCollection).find('#mat-checkbox-93-input').click();
     cy.get(CollectionLocators.closeCollectionModalBtn).click();
     cy.wait(1000);
     cy.get(CollectionLocators.collectionBtn).click();
