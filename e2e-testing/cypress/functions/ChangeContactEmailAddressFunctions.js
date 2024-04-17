3167b7331d1cfc2586d599f313d374cbf87975bc
Walter <w.obadha@gmail.com>
2024-04-17 15:22:45 +0300

    add check contact email change test

--- e2e-testing/cypress/functions/ChangeContactEmailAddressFunctions.js
+++ e2e-testing/cypress/functions/ChangeContactEmailAddressFunctions.js
@@ -1,26 +1,25 @@
 import GeneralSettingsLocator from '../locators/GeneralSettingsLocator';
 
 class ChangeContactEmailAddressFunctions {
-    open_general_settings() {
-      cy.get(GeneralSettingsLocator.settingsBtn).click(); 
-    }
-    change_contact_email_address(){
-        cy.get(GeneralSettingsLocator.emailField)
-        .clear()
-        .type("testnew@gmail.com")
-    }
-
-    click_save_button() {
-      cy.get(GeneralSettingsLocator.saveButton).click();
-    }
-
-    check_new_contact_email_address_shows(){
-        cy.get(GeneralSettingsLocator.contactEmail).contains('testnew@gmail.com')
-    }
-
-    reload_page(){
-      cy.reload()
-    }
+  open_general_settings() {
+    cy.get(GeneralSettingsLocator.settingsBtn).click();
+  }
+  change_contact_email_address() {
+    cy.get(GeneralSettingsLocator.emailField).should('not.be.empty');
+    cy.get(GeneralSettingsLocator.emailField).clear().type('testnew@gmail.com');
   }
 
-  export default ChangeContactEmailAddressFunctions;
+  click_save_button() {
+    cy.get(GeneralSettingsLocator.saveButton).click();
+  }
+
+  check_new_contact_email_address_shows() {
+    cy.get(GeneralSettingsLocator.contactEmail).contains('testnew@gmail.com');
+  }
+
+  reload_page() {
+    cy.reload();
+  }
+}
+
+export default ChangeContactEmailAddressFunctions;
