c578037fd777e100c978457bce82e79d006eb1d6
Shakira <shakira@Shakiras-MacBook-Pro.local>
2024-04-18 22:36:27 +0300

    Remove unnecessary changes

--- e2e-testing/cypress/support/commands.js
+++ e2e-testing/cypress/support/commands.js
@@ -23,6 +23,3 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-const compareSnapshotCommand = require('cypress-image-diff-js/dist/command');
-
-compareSnapshotCommand();
