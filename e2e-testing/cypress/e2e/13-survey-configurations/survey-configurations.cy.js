03c89cd8a793b0099b2c07b4402a5fed0863064f
Walter <w.obadha@gmail.com>
2024-04-18 09:08:17 +0300

    adjust hidden author test

--- e2e-testing/cypress/e2e/13-survey-configurations/survey-configurations.cy.js
+++ e2e-testing/cypress/e2e/13-survey-configurations/survey-configurations.cy.js
@@ -10,7 +10,7 @@ describe("Automated Tests for Survey Configurations", () => {
     cy.visit(Cypress.env('baseUrl'));
   });
 
-  it("Verify posts go into Published state as configured", () => {
+  it.skip("Verify posts go into Published state as configured", () => {
     surveyConfigurationFunctions.require_posts_reviewed_before_published()
   });
 
