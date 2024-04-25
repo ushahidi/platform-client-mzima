2f7170ffc4777169cce09a9c2db30e8f6aa1d92d
Anna Iosif <anna@ushahidi.com>
2024-04-25 16:21:00 +0200

    Selecting and deselecting children when selecting parent

--- apps/web-mzima-client/src/app/settings/categories/categories.component.ts
+++ apps/web-mzima-client/src/app/settings/categories/categories.component.ts
@@ -86,5 +86,17 @@ export class CategoriesComponent {
     } else {
       this.selectedCategories = this.selectedCategories.filter((sC) => sC.id !== cat.id);
     }
+    if (cat.children.length) {
+      cat.children.forEach((child) => {
+        if (i < 0) {
+          const c = this.selectedCategories.findIndex((sC) => sC.id === child.id);
+          if (c < 0) {
+            this.selectedCategories.push(child);
+          }
+        } else {
+          this.selectedCategories = this.selectedCategories.filter((sC) => sC.id !== child.id);
+        }
+      });
+    }
   }
 }
