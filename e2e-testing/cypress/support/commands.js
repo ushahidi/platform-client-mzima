10fc4ee9f5dc9cad271ae53c662dce158a78a620
Walter <w.obadha@gmail.com>
2024-05-01 22:39:08 +0300

    comment out unused plugin throwing error

--- e2e-testing/cypress/support/commands.js
+++ e2e-testing/cypress/support/commands.js
@@ -23,5 +23,5 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-const compareSnapshotCommand = require('cypress-image-diff-js/dist/command');
-compareSnapshotCommand();
+// const compareSnapshotCommand = require('cypress-image-diff-js/dist/command');
+// compareSnapshotCommand();
