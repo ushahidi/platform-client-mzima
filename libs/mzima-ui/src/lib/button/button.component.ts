4264f69ee0305ae8d1e3a360bee429eb4c53cd26
kivuvarosekivuvan <rosekivuvan@gmail.com>
2024-06-10 16:30:18 +0300

    Reverted the styling for the child div--listItem to the initial state and solved the alignment issue on mzima button--Expand-true

--- libs/mzima-ui/src/lib/button/button.component.ts
+++ libs/mzima-ui/src/lib/button/button.component.ts
@@ -6,7 +6,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
-  @Input() public expand = false;
+  @Input() public expand = true;
   @Input() public rounded = false;
   @Input() public disabled = false;
   @Input() public iconOnly = false;
