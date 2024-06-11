f39ae40b1cd8c2d5106a8e1035d30c838909f2ab
kivuvarosekivuvan <rosekivuvan@gmail.com>
2024-06-11 17:13:09 +0300

    Made changes on naming of the size

--- libs/mzima-ui/src/lib/button/button.component.ts
+++ libs/mzima-ui/src/lib/button/button.component.ts
@@ -19,7 +19,7 @@ export class ButtonComponent {
   @Input() public ariaLabel: string;
   @Input() public dataQa: string;
   @Input() public type: 'button' | 'submit' = 'button';
-  @Input() public size: 'small' | 'medium' | 'big' | 'uncertain' = 'medium';
+  @Input() public size: 'small' | 'medium' | 'big' | 'unspecified' = 'medium';
   @Input() public fill: 'solid' | 'outline' | 'clear' = 'solid';
   @Input() public color:
     | 'primary'
