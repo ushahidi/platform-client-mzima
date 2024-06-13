71bbe2d02659b5c5c6422390641cbe8de98ce85c
Shakira <shakira@Shakiras-MacBook-Pro.local>
2024-06-13 08:34:10 +0300

    Implement feedback on post details

--- e2e-testing/cypress/e2e/12-data-view/data-view.cy.js
+++ e2e-testing/cypress/e2e/12-data-view/data-view.cy.js
@@ -1,9 +1,11 @@
 import LoginFunctions from '../../functions/LoginFunctions';
 import DataViewFilterFunctions from '../../functions/DataViewFilterFunctions';
+import PostFunctions from '../../functions/PostsFunctions/PostFunctions';
 
 describe('Automated Tests for Post Filters in Data View', () => {
   const loginFunctions = new LoginFunctions();
   const dataViewFilterFunctions = new DataViewFilterFunctions();
+  const postFunctions = new PostFunctions();
 
   beforeEach(() => {
     loginFunctions.login_as_admin();
@@ -22,6 +24,9 @@ describe('Automated Tests for Post Filters in Data View', () => {
   });
 
   it('Steps for post details on data_view', () => {
+    postFunctions.open_post_creation_form();
+    postFunctions.fill_required_form_fields();
+    postFunctions.complete_add_post_steps();
     dataViewFilterFunctions.click_data_view_btn();
     dataViewFilterFunctions.post_details_data_view();
   });
