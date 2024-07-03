97e149dd2287a50b2c979c0f9a7c61791e163989
kivuvarosekivuvan <rosekivuvan@gmail.com>
2024-07-03 14:25:53 +0300

    Integrated pa11y to the project

---
+++ apps/web-mzima-client/pa11y.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    timeout: 10000,
+    headers: {
+      "User-Agent": "Pa11y"
+    },
+    includeNotices: true,
+    includeWarnings: true,
+    log: {
+      debug: console.log,
+      error: console.error,
+      info: console.log
+    },
+    standard: 'WCAG2', 
+    reporter: 'cli', 
+    ignore: [],
+    threshold: {
+      errors: 0, 
+      warnings: 0 
+    }
+  };
