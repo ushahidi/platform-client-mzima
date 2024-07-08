dd7dee446d39251a7b6f61f947555d821ab759a3
Shakira <shakira@Shakiras-MacBook-Pro.local>
2024-07-08 14:26:40 +0300

    Fix failing tests

--- e2e-testing/cypress/functions/DataViewFunctions/DataViewFilterFunctions.js
+++ e2e-testing/cypress/functions/DataViewFunctions/DataViewFilterFunctions.js
@@ -149,9 +149,15 @@ class DataViewFilterFunctions {
     cy.get('.mat-tree-node').eq(0).should('exist');
     cy.get(`[aria-level="2"]`).should('exist');
     //check that when parent elements are selected children elements are also selected
-    cy.get('.mat-tree-node').eq(0).find('.mat-checkbox-input').check({ force: true });
-    cy.get('.mat-tree-node').eq(0).find('.mat-checkbox-input').should('be.selected');
-    cy.get(`[aria-level="2"]`).eq(0).find('.mat-checkbox-input').should('be.selected');
+    cy.get('.mat-tree-node').eq(0).find('.mat-checkbox-input').click({ force: true });
+    cy.get('.mat-tree-node')
+      .eq(0)
+      .find('.mat-checkbox-input')
+      .should('have.attr', 'aria-checked', true);
+    cy.get(`[aria-level="2"]`)
+      .eq(0)
+      .find('.mat-checkbox-input')
+      .should('have.attr', 'aria-checked', true);
     //verify count shows how many categories are selected
     cy.get(DataViewLocators.selectedFilterCount).contains('3');
     //verify count of posts is updated correctly
