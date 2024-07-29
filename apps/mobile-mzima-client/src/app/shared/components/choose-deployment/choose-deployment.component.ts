a2feb741169e49cf56c9968bf03d2b7e6ec41af8
Maxwell Mwandigha <maxmwandigha@gmail.com>
2024-07-29 18:25:54 +0300

    Toast message changes depending on number of deps selected

--- apps/mobile-mzima-client/src/app/shared/components/choose-deployment/choose-deployment.component.ts
+++ apps/mobile-mzima-client/src/app/shared/components/choose-deployment/choose-deployment.component.ts
@@ -227,11 +227,23 @@ export class ChooseDeploymentComponent {
     this.addButtonVisible = false;
     this.loadDeployments();
 
+    const deploymentCount = this.selectedDeployments.length;
+    const header =
+      deploymentCount > 1
+        ? `${deploymentCount} Deployments Added Successfully!`
+        : `1 Deployment Added Successfully!`;
+    const message =
+      deploymentCount > 1
+        ? 'You can now view these deployments and add posts to them'
+        : 'You can now view this deployment and add posts to it';
+
     this.toastService.presentToast({
-      header: 'Deployment Added Successfully!',
-      message: 'You can now view this deployment and add posts to it',
+      header: header,
+      message: message,
       buttons: [],
     });
+
+    this.selectedDeployments = [];
   }
 
   public backHandle(): void {
