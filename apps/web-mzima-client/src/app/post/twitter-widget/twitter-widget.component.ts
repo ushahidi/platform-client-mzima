714c91f15a825cc3df639c6d1a188a7d320ef2bd
Ifycode <marychord@gmail.com>
2024-08-07 01:16:12 +0100

    Rough work: Almost there... + experiements

--- apps/web-mzima-client/src/app/post/twitter-widget/twitter-widget.component.ts
+++ apps/web-mzima-client/src/app/post/twitter-widget/twitter-widget.component.ts
@@ -6,6 +6,8 @@ import {
   Input,
   OnInit,
   Output,
+  OnChanges,
+  SimpleChanges,
 } from '@angular/core';
 import { TwitterService } from '../../core/services/twitter.service';
 
@@ -14,11 +16,14 @@ import { TwitterService } from '../../core/services/twitter.service';
   templateUrl: './twitter-widget.component.html',
   styleUrls: ['./twitter-widget.component.scss'],
 })
-export class TwitterWidgetComponent implements OnInit {
+export class TwitterWidgetComponent implements OnInit, OnChanges {
   @Input() public id: string;
+  public postDetailsId = '';
   public tweet: object;
 
   @Output() loadingFailed = new EventEmitter();
+  @Output() twitter_postDetailsId = new EventEmitter<string>();
+
   isTwitterScriptLoading = true;
   isTwitterFailed = false;
 
@@ -26,18 +31,37 @@ export class TwitterWidgetComponent implements OnInit {
     private readonly _elementRef: ElementRef,
     private readonly twitterService: TwitterService,
     private readonly _changeDetectorRef: ChangeDetectorRef,
-  ) {}
+  ) {
+    // console.log('constuctor: ', this.id);
+    // this._loadTwitterScript();
+    // this.isTwitterScriptLoading = true;
+    // this._updateTwitterScriptLoadingState();
+    // this.isTwitterScriptLoading = true;
+  }
 
   public ngOnInit(): void {
-    this._loadTwitterScript();
+    console.log('Husky pre-commit hook - Let my people go...');
+    // this._loadTwitterScript();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('testing...');
+    if (changes['id']) {
+      console.log('onChanges: ', this.id);
+      this.twitter_postDetailsId.emit(this.id);
+      this.postDetailsId = this.id;
+      this._loadTwitterScript();
+    }
   }
 
   private _loadTwitterScript(): void {
     this.twitterService.loadScript().subscribe((twitterData: any) => {
       this._updateTwitterScriptLoadingState();
+
       twitterData.widgets.createTweet(this.id, this._elementRef.nativeElement, {}).then(
         (tweet: any) => {
           this.tweet = tweet;
+          console.log(this.tweet);
           this.isTwitterScriptLoading = false;
         },
         () => {
