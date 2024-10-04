148b24bb3707dad1b71ff7479c0b202c139181d1
Anna Iosif <anna@ushahidi.com>
2024-10-04 10:42:10 +0200

    Using only-parameters for adding a post

--- apps/web-mzima-client/src/app/post/add-post-modal/add-post-modal.component.ts
+++ apps/web-mzima-client/src/app/post/add-post-modal/add-post-modal.component.ts
@@ -1,30 +1,37 @@
-import { Component } from '@angular/core';
-import { SurveysService, SurveyItem, generalHelpers } from '@mzima-client/sdk';
-
+import { Component, OnInit } from '@angular/core';
+import { SurveysService, generalHelpers } from '@mzima-client/sdk';
+import { Observable, of } from 'rxjs';
 @Component({
   selector: 'app-add-post-modal',
   templateUrl: './add-post-modal.component.html',
   styleUrls: ['./add-post-modal.component.scss'],
 })
-export class AddPostModalComponent {
-  public types: SurveyItem[];
+export class AddPostModalComponent implements OnInit {
+  public surveys: Observable<any>;
 
-  constructor(private surveysService: SurveysService) {
-    this.getPostAllowedTypes();
+  constructor(private surveysService: SurveysService) {}
+  ngOnInit() {
+    this.getSurveys();
   }
 
-  private getPostAllowedTypes(): void {
-    this.surveysService.get().subscribe({
-      next: (types) => {
-        this.types = types.results || [];
-        this.types.map((el) => {
-          el.visible =
-            el.everyone_can_create ||
-            el.can_create.includes(
-              localStorage.getItem(`${generalHelpers.CONST.LOCAL_STORAGE_PREFIX}role`),
-            );
-        });
-      },
+  private getSurveys(): void {
+    this.surveysService
+      .getSurveys('', { only: 'name,color,everyone_can_create,can_create' })
+      .subscribe(({ results }) => {
+        let surveys = this.mapVisibility(results);
+        surveys = surveys.filter((survey: any) => survey.visible === true);
+        this.surveys = of(surveys);
+      });
+  }
+
+  private mapVisibility(surveys: any) {
+    return surveys.map((el: any) => {
+      el.visible =
+        el.everyone_can_create ||
+        el.can_create.includes(
+          localStorage.getItem(`${generalHelpers.CONST.LOCAL_STORAGE_PREFIX}role`),
+        );
+      return el;
     });
   }
 }
