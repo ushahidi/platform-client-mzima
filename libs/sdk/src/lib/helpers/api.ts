336fe667d59fb9d910f36f970d01efb6bc93b8be
Ifycode <marychord@gmail.com>
2024-12-03 03:49:53 +0100

    Fix: Add only parameters for savedfilters

--- libs/sdk/src/lib/helpers/api.ts
+++ libs/sdk/src/lib/helpers/api.ts
@@ -14,10 +14,12 @@ export const ONLY = {
   NAME_ID_COLOR: 'name,id,color',
   NAME_COLOR_PERMISSIONS: 'name,color,everyone_can_create,can_create',
   NAME_ID_DESCRIPTION: 'name,id,description',
-  NEEDED_POSTS_LIST_PROPERTIES: 'id,title,status,color,contact,locks,post_media,data_source_message_id,post_date',
+  NEEDED_POSTS_LIST_PROPERTIES:
+    'id,title,status,color,contact,locks,post_media,data_source_message_id,post_date',
   TAG_ID_PARENTID_PARENT_SLUG: 'tag,id,parent_id,parent,slug',
   TAG_ID_PARENTID_PARENT_SLUG_CHILDREN: 'tag,id,parent_id,parent,slug,children',
   TAG_ID_PARENTID_CHILDREN: 'tag,id,parent_id,children',
+  SAVEDFILTERS_NAME_ID_FILTER: 'id,name,filter',
 };
 
 export const API_V_3 = `api/v3/`;
