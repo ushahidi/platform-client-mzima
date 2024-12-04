9bb6920c4aa54f8a131cadb582ef14ae48e0bebd
Ifycode <marychord@gmail.com>
2024-12-04 21:35:37 +0100

    Fix: Add content to post's only parameters to show post description

--- libs/sdk/src/lib/helpers/api.ts
+++ libs/sdk/src/lib/helpers/api.ts
@@ -15,7 +15,7 @@ export const ONLY = {
   NAME_COLOR_PERMISSIONS: 'name,color,everyone_can_create,can_create',
   NAME_ID_DESCRIPTION: 'name,id,description',
   NEEDED_POSTS_LIST_PROPERTIES:
-    'id,title,status,color,contact,locks,post_media,data_source_message_id,post_date',
+    'id,title,content,status,color,contact,locks,post_media,data_source_message_id,post_date',
   TAG_ID_PARENTID_PARENT_SLUG: 'tag,id,parent_id,parent,slug',
   TAG_ID_PARENTID_PARENT_SLUG_CHILDREN: 'tag,id,parent_id,parent,slug,children',
   TAG_ID_PARENTID_CHILDREN: 'tag,id,parent_id,children',
