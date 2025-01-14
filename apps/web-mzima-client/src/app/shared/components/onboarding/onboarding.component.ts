07f2a999d27aa1dca8695563895c9d239f15f8eb
Maxwell Mwandigha <47903907+MMwandigha@users.noreply.github.com>
2025-01-14 12:24:22 +0300

    [USH-1706] Onboarding guide stops progressing after 'filtering' modal (#1508)
    * Delay for dynamic steps to ensure DOM updates before highlighting
    
    * Slight cleanup
    
    * test
    
    * Removing unused code

--- apps/web-mzima-client/src/app/shared/components/onboarding/onboarding.component.ts
+++ apps/web-mzima-client/src/app/shared/components/onboarding/onboarding.component.ts
@@ -57,12 +57,12 @@ export class OnboardingComponent implements AfterViewInit {
           this.router.navigate(['/settings']);
         }
 
-        // if (this.onboardingSteps[data.order].dynamic) {
-        //   setTimeout(() => {
-        //     this.customTourService.updateHighlightedElements();
-        //     this.isHidden = false;
-        //   }, 1000);
-        // }
+        if (this.onboardingSteps[data.order].dynamic) {
+          setTimeout(() => {
+            this.customTourService.updateHighlightedElements();
+            this.isHidden = false;
+          }, 1000);
+        }
       },
     });
 
@@ -95,15 +95,6 @@ export class OnboardingComponent implements AfterViewInit {
 
     this.eventBusService.on(EventType.ShowOnboarding).subscribe({
       next: () => this.initOnboarding(),
-    });
-
-    this.eventBusService.on(EventType.FeedPostsLoaded).subscribe({
-      next: () => {
-        setTimeout(() => {
-          this.customTourService.updateHighlightedElements();
-          this.isHidden = false;
-        }, 100);
-      },
     });
 
     this.sessionService.isFiltersVisible$.pipe(untilDestroyed(this)).subscribe({
