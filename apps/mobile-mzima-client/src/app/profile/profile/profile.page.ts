c9a8ecb6229a880c711d0503bbbb7bc06d14bd0c
Anna Iosif <anna.iosif@me.com>
2025-01-17 12:44:58 +0100

    Hiding Intercom functionality in mobile app (#1511)
    * Removing intercom-window from app
    
    * Adjustment to text

--- apps/mobile-mzima-client/src/app/profile/profile/profile.page.ts
+++ apps/mobile-mzima-client/src/app/profile/profile/profile.page.ts
@@ -46,14 +46,15 @@ export class ProfilePage {
         this.profileMenu = profileMenu.profileMenu.filter(
           (i) => i.isLoggedGuard === undefined || i.isLoggedGuard === !!userData.userId,
         );
-        if (userData.userId)
-          this.supportItems.push(<SupportItem>{
-            title: 'Intercom',
-            description: 'Contact Ushahidi staff for chat support',
-            action: () => {
-              this.intercomService.displayMessenger();
-            },
-          });
+        // if (userData.userId) {
+        // this.supportItems.push(<SupportItem>{
+        //   title: 'Intercom',
+        //   description: 'Contact Ushahidi staff for chat support',
+        //   action: () => {
+        //     this.intercomService.displayMessenger();
+        //   },
+        // });
+        // }
       });
   }
 
