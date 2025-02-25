dbb7aa59c9fd171289a3be8a69b97b18c62c564d
Ifycode <marychord@gmail.com>
2025-02-25 14:51:18 +0100

    Fix: Remove delete and comment

--- libs/sdk/src/lib/services/posts.service.ts
+++ libs/sdk/src/lib/services/posts.service.ts
@@ -365,9 +365,6 @@ export class PostsService extends ResourceService<any> {
       }
     }
     if (updated) {
-      // This is where it deletes them from...?
-      delete newFilters.date_before;
-      delete newFilters.date_after;
       this.postsFilters.next(newFilters);
     }
   }
