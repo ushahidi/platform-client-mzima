851a1102ceb26726eb834e91322ee21373d3f023
Anna Iosif <anna@ushahidi.com>
2025-02-26 16:44:20 +0100

    Basic popup with edit and detailed view working on map

--- apps/web-mzima-client/src/app/map/map.component.ts
+++ apps/web-mzima-client/src/app/map/map.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit, ViewContainerRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { debounceTime, Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+
 import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
@@ -10,22 +12,22 @@ import VectorLayer from 'ol/layer/Vector';
 import Style from 'ol/style/Style';
 import Fill from 'ol/style/Fill';
 import CircleStyle from 'ol/style/Circle';
+import Icon from 'ol/style/Icon';
 import Overlay from 'ol/Overlay';
-import { mapHelper } from '@helpers';
+import { mapHelper, searchFormHelper } from '@helpers';
 import XYZ from 'ol/source/XYZ';
 import LayerSwitcher from 'ol-layerswitcher';
 import { BaseLayerOptions, GroupLayerOptions } from 'ol-layerswitcher';
 import LayerGroup from 'ol/layer/Group';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { MainViewComponent } from '@shared';
-import {
-  SavedsearchesService,
-  PostsService,
-  GeoJsonPostsResponse,
-} from '@mzima-client/sdk';
+import { SavedsearchesService, PostsService, GeoJsonPostsResponse } from '@mzima-client/sdk';
 import GeoJSON from 'ol/format/GeoJSON';
 import { SessionService, EventBusService, EventType, BreakpointService } from '@services';
 import { fromLonLat } from 'ol/proj';
+import { get, over } from 'lodash';
+import { PostDetailsModalComponent } from './post-details-modal/post-details-modal.component';
+import { PostPreviewComponent } from '../post/post-preview/post-preview.component';
 
 @UntilDestroy()
 @Component({
@@ -39,6 +41,7 @@ export class MapComponent extends MainViewComponent implements OnInit {
   filtersSubscription$: Observable<any>;
   baseMaps: LayerGroup;
   surveyLayer!: VectorLayer;
+  post: any;
 
   constructor(
     protected override router: Router,
@@ -48,31 +51,45 @@ export class MapComponent extends MainViewComponent implements OnInit {
     protected override eventBusService: EventBusService,
     protected override sessionService: SessionService,
     protected override breakpointService: BreakpointService,
+    private dialog: MatDialog,
+    private view: ViewContainerRef,
+    private zone: NgZone,
   ) {
-    super(router, route, postsService, savedSearchesService, eventBusService, sessionService, breakpointService);
+    super(
+      router,
+      route,
+      postsService,
+      savedSearchesService,
+      eventBusService,
+      sessionService,
+      breakpointService,
+    );
     this.filtersSubscription$ = this.postsService.postsFilters$.pipe(untilDestroyed(this));
   }
 
   ngOnInit(): void {
     this.mapConfig = this.sessionService.getMapConfigurations();
-    this.initFilterListener();
     this.initBaseLayers();
     this.initMap();
     this.loadData();
   }
 
   initBaseLayers() {
-    const baseLayers = mapHelper.getOpenLayersMapConfig()
-      .filter(layer => layer.visible)
-      .map(layer => new TileLayer({
-        title: layer.name,
-        type: 'base',
-        visible: this.mapConfig.default_view?.baselayer === layer.code,
-        source: new XYZ({
-          url: layer.url,
-          maxZoom: 'maxZoom' in layer.layerOptions ? layer.layerOptions.maxZoom : undefined
-        })
-      } as BaseLayerOptions));
+    const baseLayers = mapHelper
+      .getOpenLayersMapConfig()
+      .filter((layer) => layer.visible)
+      .map(
+        (layer) =>
+          new TileLayer({
+            title: layer.name,
+            type: 'base',
+            visible: this.mapConfig.default_view?.baselayer === layer.code,
+            source: new XYZ({
+              url: layer.url,
+              maxZoom: 'maxZoom' in layer.layerOptions ? layer.layerOptions.maxZoom : undefined,
+            }),
+          } as BaseLayerOptions),
+      );
 
     this.baseMaps = new LayerGroup({
       title: 'Base Maps',
@@ -98,10 +115,8 @@ export class MapComponent extends MainViewComponent implements OnInit {
       groupSelectStyle: 'children',
       startActive: true,
       activationMode: 'click',
-      label:'',
+      label: '',
       collapseLabel: '',
-
-      
     });
 
     this.map.addControl(layerSwitcher);
@@ -109,23 +124,13 @@ export class MapComponent extends MainViewComponent implements OnInit {
   }
 
   loadData(): void {
-    this.getPostsGeoJson();
-  }
-
-  private initFilterListener() {
-    this.filtersSubscription$.pipe(debounceTime(1000)).subscribe({
-      next: (filter) => {
-        if (this.route.snapshot.data['view'] === 'search' && !this.searchId) return;
-        if (this.route.snapshot.data['view'] === 'collection' && !this.collectionId) return;
-
-        this.getPostsGeoJson(1, filter);
-      },
-    });
+    const defaultFilters = searchFormHelper.DEFAULT_FILTERS_LOGGED_OUT;
+    this.getPostsGeoJson(1, defaultFilters);
   }
 
   getPostsGeoJson(pageNumber: number = 1, filter?: any) {
     this.postsService
-      .getGeojson({ limit: 500, currentView: 'map', page: pageNumber })
+      .getGeojson(filter)
       .pipe(untilDestroyed(this))
       .subscribe({
         next: (posts: GeoJsonPostsResponse) => {
@@ -142,13 +147,14 @@ export class MapComponent extends MainViewComponent implements OnInit {
                   source: r.source,
                   title: r.title,
                 },
-                geometry: f.geometry
-              }))
-            )
+                geometry: f.geometry,
+              })),
+            ),
           };
-
           const style = (feature: any) => {
-            const color = feature.getProperties().color ? `#${feature.getProperties().color}` : '#ffff';
+            const color = feature.getProperties().color
+              ? `#${feature.getProperties().color}`
+              : '#ffff';
             return new Style({
               image: new CircleStyle({
                 fill: new Fill({ color }),
@@ -156,6 +162,21 @@ export class MapComponent extends MainViewComponent implements OnInit {
               }),
             });
           };
+
+          // const style = (feature: any) => {
+          //   // const color = feature.getProperties().color || 'fff';
+          //   // const marker = mapHelper.pointIcon(color);
+          //   // const src = typeof marker.options.html === 'string' ? 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(marker.options.html) : '';
+
+          //   return new Style({
+          //     image: new Icon({
+          //       src: './assets/icons/marker.svg',
+          //       offset: [0, 0],
+          //       opacity: 1,
+          //       scale: 0.35,
+          //     }),
+          //   });
+          // };
 
           const vectorSource = new VectorSource({
             features: new GeoJSON().readFeatures(geoJson, {
@@ -172,16 +193,14 @@ export class MapComponent extends MainViewComponent implements OnInit {
         },
         error: (error) => {
           console.error('Error loading posts geojson', error);
-        }
+        },
       });
   }
 
   initiatePopups() {
     const container = document.getElementById('popup');
-    const content = document.getElementById('popup-content');
-    const closer = document.getElementById('popup-closer');
 
-    if (!container || !content || !closer) return;
+    if (!container) return;
 
     const overlay = new Overlay({
       element: container,
@@ -192,28 +211,69 @@ export class MapComponent extends MainViewComponent implements OnInit {
       },
     });
 
-    closer.onclick = () => {
-      overlay.setPosition(undefined);
-      closer.blur();
-      return false;
-    };
-
     this.map.addOverlay(overlay);
-
     this.map.on('click', (evt) => {
       const coordinate = evt.coordinate;
-      const feature = this.map.forEachFeatureAtPixel(evt.pixel, (feature: any, layer: any) => {
-        return layer === this.surveyLayer ? feature : null;
+      const feature = this.map.forEachFeatureAtPixel(evt.pixel, (feat: any, layer: any) => {
+        return layer === this.surveyLayer ? feat : null;
       });
-
       if (feature) {
-        const { title, description, color } = feature.getProperties();
-        container.style.borderLeft = `10px solid #${color || 'fff'}`;
-        content.innerHTML = `<h3>${title}</h3><p>${description}</p>`;
-        overlay.setPosition(coordinate);
-      } else {
-        overlay.setPosition(undefined);
+        const { id } = feature.getProperties();
+        if (id) {
+          const comp = this.view.createComponent(PostPreviewComponent);
+
+          this.postsService.getById(id).subscribe({
+            next: (post) => {
+              comp.setInput('post', post);
+              comp.setInput('user', this.user);
+              this.postsService.getById(id).subscribe({
+                next: (postV5) => {
+                  overlay.setElement(comp.location.nativeElement);
+                  comp.instance.details$.subscribe({
+                    next: () => {
+                      this.showPostDetailsModal(postV5, post.color, post.data_source_message_id);
+                    },
+                  });
+                  comp.instance.edit.subscribe({
+                    next: () => {
+                      this.showPostDetailsModal(
+                        postV5,
+                        post.color,
+                        post.data_source_message_id,
+                        true,
+                      );
+                    },
+                  });
+
+                  overlay.setPosition(coordinate);
+                },
+              });
+            },
+            error: (error) => {
+              console.error('Error fetching post details', error);
+            },
+          });
+          overlay.setPosition(coordinate);
+        } else {
+          overlay.setPosition(undefined);
+        }
       }
+    });
+  }
+
+  private showPostDetailsModal(
+    post: any,
+    color: string,
+    twitterId?: string,
+    editable?: boolean,
+  ): void {
+    this.dialog.open(PostDetailsModalComponent, {
+      width: '100%',
+      maxWidth: 576,
+      data: { post, color, twitterId, editable },
+      height: 'auto',
+      maxHeight: '90vh',
+      panelClass: ['modal', 'post-modal'],
     });
   }
 }
