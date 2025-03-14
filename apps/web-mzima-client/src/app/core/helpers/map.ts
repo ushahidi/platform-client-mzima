fe7a7b2eda45efcbed5573d8c89f4ea3dc2f52ca
Anna Iosif <anna@ushahidi.com>
2025-03-14 11:02:22 +0100

    Removing repetition from wms-functions

--- apps/web-mzima-client/src/app/core/helpers/map.ts
+++ apps/web-mzima-client/src/app/core/helpers/map.ts
@@ -120,37 +120,25 @@ interface WaterLayer {
 }
 
 export const getWaterLayer = (): WaterLayer[] => {
+  const createWaterLayer = (name: string, time: string): WaterLayer => ({
+    name,
+    url: 'https://ows.digitalearth.africa/wms?version=1.3.0',
+    attribution:
+      '<br><br><strong>Water Observations from Space (WOfS)</strong> is a service that uses satellite images to provide historical surface water observations across the African continent. WOfS enables users to understand the location and movement of inland and coastal water over time. It shows where water is usually present; where it is seldom observed; and where inundation of the surface has been observed by satellite. The product used is accessible through <a href="https://ows.digitalearth.africa/" target="_blank">OGC Web Service</a>. For more information, see the <a href="https://docs.digitalearthafrica.org/en/latest/data_specs/Landsat_WOfS_specs.html" target="_blank">Digital Earth Africa User Guide.</a>',
+    params: {
+      LAYERS: 'wofs_ls_summary_annual',
+      FORMAT: 'image/png',
+      TRANSPARENT: true,
+      TIME: time,
+      CRS: 'EPSG:3857',
+      exceptions: 'XML',
+      styles: 'wofs_summary_annual_frequency',
+    },
+  });
+
   return [
-    {
-      name: 'Water Observations from space 2023',
-      url: 'https://ows.digitalearth.africa/wms?version=1.3.0',
-      attribution:
-        '<br><br><strong>Water Observations from Space (WOfS)</strong> is a service that uses satellite images to provide historical surface water observations across the African continent. WOfS enables users to understand the location and movement of inland and coastal water over time. It shows where water is usually present; where it is seldom observed; and where inundation of the surface has been observed by satellite. The product used is accessible through <a href="https://ows.digitalearth.africa/" target="_blank">OGC Web Service</a>. For more information, see the <a href="https://docs.digitalearthafrica.org/en/latest/data_specs/Landsat_WOfS_specs.html" target="_blank">Digital Earth Africa User Guide.</a>',
-      params: {
-        LAYERS: 'wofs_ls_summary_annual',
-        FORMAT: 'image/png',
-        TRANSPARENT: true,
-        TIME: '2023-01-01',
-        CRS: 'EPSG:3857',
-        exceptions: 'XML',
-        styles: 'wofs_summary_annual_frequency',
-      },
-    },
-    {
-      name: 'Water Observations from space 2013',
-      url: 'https://ows.digitalearth.africa/wms?version=1.3.0',
-      attribution:
-        '<br><br><strong>Water Observations from Space (WOfS)</strong> is a service that uses satellite images to provide historical surface water observations across the African continent. WOfS enables users to understand the location and movement of inland and coastal water over time. It shows where water is usually present; where it is seldom observed; and where inundation of the surface has been observed by satellite. The product used is accessible through <a href="https://ows.digitalearth.africa/" target="_blank">OGC Web Service</a>. For more information, see the <a href="https://docs.digitalearthafrica.org/en/latest/data_specs/Landsat_WOfS_specs.html" target="_blank">Digital Earth Africa User Guide.</a>',
-      params: {
-        LAYERS: 'wofs_ls_summary_annual',
-        FORMAT: 'image/png',
-        TRANSPARENT: true,
-        TIME: '2013-01-01',
-        CRS: 'EPSG:3857',
-        exceptions: 'XML',
-        styles: 'wofs_summary_annual_frequency',
-      },
-    },
+    createWaterLayer('Water Observations from space 2023', '2023-01-01'),
+    createWaterLayer('Water Observations from space 2013', '2013-01-01'),
   ];
 };
 
@@ -170,171 +158,22 @@ interface RainfallLayer {
 }
 
 export const getRainfallLayer = (): RainfallLayer[] => {
-  return [
-    {
-      name: 'Rainfall january 1981',
-      url: 'https://ows.digitalearth.africa/wms?version=1.3.0',
-      attribution:
-        '<br><br><strong>CHIRPS</strong> is a <strong>USGS and Climate Hazards Centre</strong> initiative with funding from <strong>USAID, NASA, and NOAA</strong>. Find out more on the <a href="https://www.chc.ucsb.edu/data/chirps." target="_blank">CHIRPS website</a>',
-      params: {
-        LAYERS: 'rainfall_chirps_monthly',
-        FORMAT: 'image/png',
-        TRANSPARENT: true,
-        TIME: '1981-01-01',
-        CRS: 'EPSG:3857',
-        exceptions: 'XML',
-        styles: 'rainfall_monthly',
-      },
+  const createRainfallLayer = (name: string, time: string): RainfallLayer => ({
+    name,
+    url: 'https://ows.digitalearth.africa/wms?version=1.3.0',
+    attribution:
+      '<br><br><strong>CHIRPS</strong> is a <strong>USGS and Climate Hazards Centre</strong> initiative with funding from <strong>USAID, NASA, and NOAA</strong>. Find out more on the <a href="https://www.chc.ucsb.edu/data/chirps." target="_blank">CHIRPS website</a>',
+    params: {
+      LAYERS: 'rainfall_chirps_monthly',
+      FORMAT: 'image/png',
+      TRANSPARENT: true,
+      TIME: time,
+      CRS: 'EPSG:3857',
+      exceptions: 'XML',
+      styles: 'rainfall_monthly',
     },
-    {
-      name: 'Rainfall january 1986',
-      attribution:
-        '<br><br><strong>CHIRPS</strong> is a <strong>USGS and Climate Hazards Centre</strong> initiative with funding from <strong>USAID, NASA, and NOAA</strong>. Find out more on the <a href="https://www.chc.ucsb.edu/data/chirps." target="_blank">CHIRPS website</a>',
-      url: 'https://ows.digitalearth.africa/wms?version=1.3.0?',
-      params: {
-        LAYERS: 'rainfall_chirps_monthly',
-        FORMAT: 'image/png',
-        TRANSPARENT: true,
-        TIME: '1986-01-01',
-        CRS: 'EPSG:3857',
-        exceptions: 'XML',
-        styles: 'rainfall_monthly',
-      },
-    },
-    {
-      name: 'Rainfall january 1991',
-      attribution:
-        '<br><br><strong>CHIRPS</strong> is a <strong>USGS and Climate Hazards Centre</strong> initiative with funding from <strong>USAID, NASA, and NOAA</strong>. Find out more on the <a href="https://www.chc.ucsb.edu/data/chirps." target="_blank">CHIRPS website</a>',
-      url: 'https://ows.digitalearth.africa/wms?version=1.3.0?',
-      params: {
-        LAYERS: 'rainfall_chirps_monthly',
-        FORMAT: 'image/png',
-        TRANSPARENT: true,
-        TIME: '1991-01-01',
-        CRS: 'EPSG:3857',
-        exceptions: 'XML',
-        styles: 'rainfall_monthly',
-      },
-    },
-    {
-      name: 'Rainfall january 1996',
-      attribution:
-        '<br><br><strong>CHIRPS</strong> is a <strong>USGS and Climate Hazards Centre</strong> initiative with funding from <strong>USAID, NASA, and NOAA</strong>. Find out more on the <a href="https://www.chc.ucsb.edu/data/chirps." target="_blank">CHIRPS website</a>',
-      url: 'https://ows.digitalearth.africa/wms?version=1.3.0?',
-      params: {
-        LAYERS: 'rainfall_chirps_monthly',
-        FORMAT: 'image/png',
-        TRANSPARENT: true,
-        TIME: '1996-01-01',
-        CRS: 'EPSG:3857',
-        exceptions: 'XML',
-        styles: 'rainfall_monthly',
-      },
-    },
-    {
-      name: 'Rainfall january 2001',
-      attribution:
-        '<br><br><strong>CHIRPS</strong> is a <strong>USGS and Climate Hazards Centre</strong> initiative with funding from <strong>USAID, NASA, and NOAA</strong>. Find out more on the <a href="https://www.chc.ucsb.edu/data/chirps." target="_blank">CHIRPS website</a>',
-      url: 'https://ows.digitalearth.africa/wms?version=1.3.0?',
-      params: {
-        LAYERS: 'rainfall_chirps_monthly',
-        FORMAT: 'image/png',
-        TRANSPARENT: true,
-        TIME: '2001-01-01',
-        CRS: 'EPSG:3857',
-        exceptions: 'XML',
-        styles: 'rainfall_monthly',
-      },
-    },
-    {
-      name: 'Rainfall january 2006',
-      attribution:
-        '<br><br><strong>CHIRPS</strong> is a <strong>USGS and Climate Hazards Centre</strong> initiative with funding from <strong>USAID, NASA, and NOAA</strong>. Find out more on the <a href="https://www.chc.ucsb.edu/data/chirps." target="_blank">CHIRPS website</a>',
-      url: 'https://ows.digitalearth.africa/wms?version=1.3.0?',
-      params: {
-        LAYERS: 'rainfall_chirps_monthly',
-        FORMAT: 'image/png',
-        TRANSPARENT: true,
-        TIME: '2006-01-01',
-        CRS: 'EPSG:3857',
-        exceptions: 'XML',
-        styles: 'rainfall_monthly',
-      },
-    },
-    {
-      name: 'Rainfall january 2011',
-      attribution:
-        '<br><br><strong>CHIRPS</strong> is a <strong>USGS and Climate Hazards Centre</strong> initiative with funding from <strong>USAID, NASA, and NOAA</strong>. Find out more on the <a href="https://www.chc.ucsb.edu/data/chirps." target="_blank">CHIRPS website</a>',
-      url: 'https://ows.digitalearth.africa/wms?version=1.3.0?',
-      params: {
-        LAYERS: 'rainfall_chirps_monthly',
-        FORMAT: 'image/png',
-        TRANSPARENT: true,
-        TIME: '2011-01-01',
-        CRS: 'EPSG:3857',
-        exceptions: 'XML',
-        styles: 'rainfall_monthly',
-      },
-    },
-    {
-      name: 'Rainfall january 2016',
-      attribution:
-        '<br><br><strong>CHIRPS</strong> is a <strong>USGS and Climate Hazards Centre</strong> initiative with funding from <strong>USAID, NASA, and NOAA</strong>. Find out more on the <a href="https://www.chc.ucsb.edu/data/chirps." target="_blank">CHIRPS website</a>',
-      url: 'https://ows.digitalearth.africa/wms?version=1.3.0?',
-      params: {
-        LAYERS: 'rainfall_chirps_monthly',
-        FORMAT: 'image/png',
-        TRANSPARENT: true,
-        TIME: '2016-12-01',
-        CRS: 'EPSG:3857',
-        exceptions: 'XML',
-        styles: 'rainfall_monthly',
-      },
-    },
-    {
-      name: 'Rainfall january 2021 ',
-      attribution:
-        '<br><br><strong>CHIRPS</strong> is a <strong>USGS and Climate Hazards Centre</strong> initiative with funding from <strong>USAID, NASA, and NOAA</strong>. Find out more on the <a href="https://www.chc.ucsb.edu/data/chirps." target="_blank">CHIRPS website</a>',
-      url: 'https://ows.digitalearth.africa/wms?version=1.3.0?',
-      params: {
-        LAYERS: 'rainfall_chirps_monthly',
-        FORMAT: 'image/png',
-        TRANSPARENT: true,
-        TIME: '2021-12-01',
-        CRS: 'EPSG:3857',
-        exceptions: 'XML',
-        styles: 'rainfall_monthly',
-      },
-    },
-    {
-      name: 'Rainfall january 2024 ',
-      attribution:
-        '<br><br><strong>CHIRPS</strong> is a <strong>USGS and Climate Hazards Centre</strong> initiative with funding from <strong>USAID, NASA, and NOAA</strong>. Find out more on the <a href="https://www.chc.ucsb.edu/data/chirps." target="_blank">CHIRPS website</a>',
-      url: 'https://ows.digitalearth.africa/wms?version=1.3.0?',
-      params: {
-        LAYERS: 'rainfall_chirps_monthly',
-        FORMAT: 'image/png',
-        TRANSPARENT: true,
-        TIME: '2024-01-01',
-        CRS: 'EPSG:3857',
-        exceptions: 'XML',
-        styles: 'rainfall_monthly',
-      },
-    },
-    {
-      name: 'Rainfall january 2025 ',
-      attribution:
-        '<br><br><strong>CHIRPS</strong> is a <strong>USGS and Climate Hazards Centre</strong> initiative with funding from <strong>USAID, NASA, and NOAA</strong>. Find out more on the <a href="https://www.chc.ucsb.edu/data/chirps." target="_blank">CHIRPS website</a>',
-      url: 'https://ows.digitalearth.africa/wms?version=1.3.0?',
-      params: {
-        LAYERS: 'rainfall_chirps_monthly',
-        FORMAT: 'image/png',
-        TRANSPARENT: true,
-        TIME: '2025-01-01',
-        CRS: 'EPSG:3857',
-        exceptions: 'XML',
-        styles: 'rainfall_monthly',
-      },
-    },
-  ];
+  });
+
+  const years = [1981, 1986, 1991, 1996, 2001, 2006, 2011, 2016, 2021, 2024, 2025];
+  return years.map((year) => createRainfallLayer(`Rainfall January ${year}`, `${year}-01-01`));
 };
